9549e8063794c3c11bd45da826b1898724533f58
Karl Trygve Kalleberg <karltk@gmail.com>
2005-11-24 01:15:42 +0000

    Added list tests to run.
    svn path=/spoofax/trunk/spoofax/core/; revision=13720

--- core/src/org/spoofax/interpreter/test/LibraryTest.java
+++ core/src/org/spoofax/interpreter/test/LibraryTest.java
@@ -18,6 +18,12 @@ public class LibraryTest extends InterpreterTest {
     public void testTuple() {  interpTest("tuple-test", "[2,0]"); }
     public void testTemplate() {  interpTest("template-test", "[1,0]"); }
     public void testReals() {  interpTest("reals-test", "[8,0]"); }
+    public void testListSet() { interpTest("list-set-test", "[25,0]"); }
+    public void testListZip() { interpTest("list-zip-test", "[3,0]"); }
+    public void testIntList() { interpTest("int-list-test", "[15,0]"); }
+    public void testListMisc() { interpTest("list-misc-test", "[23,0]"); }
+    public void testListBasic() { interpTest("list-basic-test" ,"[17,0]"); }
+    public void testListIndex() { interpTest("list-index-test", "[17,0]"); }
     
 /*    public void testPOSIXError() {  interpTest("posix-error-test");   }
     
@@ -25,29 +31,23 @@ public class LibraryTest extends InterpreterTest {
     public void testSubstitution() {  interpTest("substitution-test");   }
     public void testListFilter() {  interpTest("lister-filter-test");   }
     public void testUnification() { interpTest("unification-test"); } 
-    public void testListZip() { interpTest("list-zip-test"); }
     public void testIO() { interpTest("io-test"); }
     public void testDir() { interpTest("dir-test"); }
     public void testEnvTraversal() { interpTest("env-traversal-test"); }
     public void testRename() { interpTest("rename-test"); }
     public void testShare() { interpTest("share-test"); }
     public void testIteration() { interpTest("iteration-test"); }
-    public void testIntList() { interpTest("int-list-test"); }
     public void testApply() { interpTest("apply-test"); }
     public void testSets() { interpTest("sets-test"); }
     public void testPOSIXFile() { interpTest("posix-file-test"); }
-    public void testListIndex() { interpTest("list-index-test"); }
     public void testPOSIXProcess() { interpTest("posix-process-test"); }
     public void testTermZip() { interpTest("term-zip-test"); }
     public void testScopedFiniteMap() { interpTest("scoped-finite-map-test"); }
     public void testStrcmp() { interpTest("strcmp-test"); }
     public void testSimpleTraversal() { interpTest("simple-traversal-test"); }
     public void testCollect() { interpTest("collect-test"); }
-    public void testListMisc() { interpTest("list-misc-test"); }
-    public void testListSet() { interpTest("list-set-test"); }
     public void testStringMisc() { interpTest("string-misc-test"); }
     public void testIntegers() { interpTest("integers-test"); }
-    public void testListBasic() { interpTest("list-basic-test"); }
     public void testTime() { interpTest("time-test"); }
     public void testParenthesize() { interpTest("parenthesize-test"); }
     public void testParseOptions() { interpTest("parse-options-test"); }
