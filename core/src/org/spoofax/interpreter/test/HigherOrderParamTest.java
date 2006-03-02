21c07649f0d6724d4ce8edc79f18d77d28f67c3b
Karl Trygve Kalleberg <karltk@gmail.com>
2006-03-02 14:10:56 +0000

    Added tests for higher-order parameters.
    svn path=/spoofax/trunk/spoofax/core/; revision=14603

---
+++ core/src/org/spoofax/interpreter/test/HigherOrderParamTest.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 02.mar.2006
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package org.spoofax.interpreter.test;
+
+public class HigherOrderParamTest extends LanguageTest {
+
+    public void testHigherOrderParam1() {
+        interpTest("higher_order_param_1", itp.makeTuple("[]"), itp.makeTerm("5"));
+    }
+
+    public void testHigherOrderParam2() {
+        interpTest("higher_order_param_2", itp.makeTuple("[]"), itp.makeTerm("5"));
+    }
+    
+    public void testHigherOrderParam3() {
+        interpTest("higher_order_param_3", itp.makeTuple("[]"), itp.makeTerm("5"));
+    }
+    
+    public void testHigherOrderParam4() {
+        interpTest("higher_order_param_4", itp.makeTuple("[]"), itp.makeTerm("5"));
+    }
+}
