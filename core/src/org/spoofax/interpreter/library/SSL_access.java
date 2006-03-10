fa60ccdbd92dd143e22d7a0cb742ac2b9fc4a88c
Karl Trygve Kalleberg <karltk@gmail.com>
2006-03-10 22:02:37 +0000

    Second argument is a list of access constructors, not an int.
    svn path=/spoofax/trunk/spoofax/core/; revision=14642

--- core/src/org/spoofax/interpreter/library/SSL_access.java
+++ core/src/org/spoofax/interpreter/library/SSL_access.java
@@ -16,46 +16,71 @@ import org.spoofax.interpreter.Tools;
 import org.spoofax.interpreter.stratego.Strategy;
 
 import aterm.ATerm;
-import aterm.ATermInt;
+import aterm.ATermAppl;
+import aterm.ATermList;
 
 public class SSL_access extends Primitive {
 
     private static final int R_OK = 4;
+
     private static final int W_OK = 2;
+
     private static final int X_OK = 1;
+
     private static final int F_OK = 0;
 
     protected SSL_access() {
         super("SSL_access", 0, 2);
     }
-    
+
     public boolean call(IContext env, List<Strategy> sargs, List<ATerm> targs) throws FatalError {
         debug("SSL_access");
-        
-        if(Tools.isATermString(targs.get(0)))
+
+        if (!Tools.isATermString(targs.get(0)))
             return false;
-        if(Tools.isATermInt(targs.get(1)))
+        if (!Tools.isATermAppl(targs.get(1)))
             return false;
 
         String path = Tools.getATermString(targs.get(0));
-        int permissions = Tools.getATermInt((ATermInt)targs.get(1));
+        int permissions = permissions_from_term((ATermList) Tools.consToList(env.getFactory(),
+                                                                             (ATermAppl) targs
+                                                                                     .get(1)));
         File f = new File(path);
-        
-        if((permissions & R_OK) != 0 ) {
-            if(!f.canRead())
+
+        if ((permissions & R_OK) != 0) {
+            if (!f.canRead())
                 return false;
-        } else if((permissions & W_OK) != 0) {
-            if(!f.canWrite())
+        } else if ((permissions & W_OK) != 0) {
+            if (!f.canWrite())
                 return false;
-        } else if((permissions & X_OK) != 0) {
+        } else if ((permissions & X_OK) != 0) {
             // FIXME: We cannot know this in Java < 1.6
             return false;
-        } else if(permissions == F_OK) {
-            if(!f.exists())
+        } else if (permissions == F_OK) {
+            if (!f.exists())
                 return false;
         }
-        
+
         env.setCurrent(targs.get(0));
         return true;
+    }
+
+    private int permissions_from_term(ATermList perms) {
+        int res = 0;
+        for (int i = 0; i < perms.getChildCount(); i++) {
+            ATermAppl t = (ATermAppl) Tools.termAt(perms, i);
+            
+            if(Tools.termType(t, "W_OK"))
+                res |= W_OK;
+            else if(Tools.termType(t, "R_OK"))
+                res |= R_OK;
+            else if(Tools.termType(t, "X_OK"))
+                res |= X_OK;
+            else if(Tools.termType(t, "F_OK"))
+                res |= F_OK;
+            else 
+                System.err.println("*** ERROR: not an access mode: " + t);
+        }
+        return res;
     }
 }
