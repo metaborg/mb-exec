dfceb571e867466ab365389d1ea5f7bc15c2a75f
Karl Trygve Kalleberg <karltk@gmail.com>
2007-01-02 03:17:07 +0000

    Added back missing sqrt.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16290

--- org.spoofax.interpreter/src/org/spoofax/interpreter/library/SSLLibrary.java
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/library/SSLLibrary.java
@@ -68,6 +68,7 @@ public class SSLLibrary extends AbstractStrategoOperatorRegistry {
         add(new SSL_cos());
         add(new SSL_sin());
         add(new SSL_sin());
+        add(new SSL_sqrt());
         add(new SSL_real_to_string());
         add(new SSL_real_to_string_precision());
         add(new SSL_string_to_real());
