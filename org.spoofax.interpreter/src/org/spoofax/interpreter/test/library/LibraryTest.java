9bcccdc13df443ede2d2991917b0a5fc021b63b6
Karl Trygve Kalleberg <karltk@gmail.com>
2007-01-02 03:27:32 +0000

    Updated expected library success counts.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16291

--- org.spoofax.interpreter/src/org/spoofax/interpreter/test/library/LibraryTest.java
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/test/library/LibraryTest.java
@@ -19,30 +19,30 @@ public class LibraryTest extends AbstractInterpreterTest {
         super.setUp("tests/data/library");
     }
 
-    public void testTuple() {  interpTest("tuple-test", "[2,0]"); }
-    public void testTemplate() {  interpTest("template-test", "[1,0]"); }
-    public void testReals() {  interpTest("reals-test", "[8,0]"); }
-    public void testListSet() { interpTest("list-set-test", "[25,0]"); }
-    public void testListZip() { interpTest("list-zip-test", "[3,0]"); }
-    public void testIntList() { interpTest("int-list-test", "[15,0]"); }
-    public void testListMisc() { interpTest("list-misc-test", "[23,0]"); }
-    public void testListIndex() { interpTest("list-index-test", "[17,0]"); }
-    public void testListFilter() {  interpTest("list-filter-test", "[2,0]");   }
-    public void testIntegers() { interpTest("integers-test", "[79,0]"); }
-    public void testIteration() { interpTest("iteration-test", "[25,0]"); }
-    public void testUnification() { interpTest("unification-test", "[2,0]"); }
-    public void testStrcmp() { interpTest("strcmp-test", "[16,0]"); }
-    public void testTermZip() { interpTest("term-zip-test", "[8,0]"); }
-    public void testEnvTraversal() { interpTest("env-traversal-test", "[2,0]"); }
-    public void testSets() { interpTest("sets-test", "[11,0]"); }
-    public void testTables() { interpTest("tables-test", "[50,0]"); }
-    public void testSubstitution() {  interpTest("substitution-test", "[1,0]");   }
+    public void testTuple() {  interpTest("tuple-test", "(2,0)"); }
+    public void testTemplate() {  interpTest("template-test", "(1,0)"); }
+    public void testReals() {  interpTest("reals-test", "(8,0)"); }
+    public void testListSet() { interpTest("list-set-test", "(25,0)"); }
+    public void testListZip() { interpTest("list-zip-test", "(3,0)"); }
+    public void testIntList() { interpTest("int-list-test", "(15,0)"); }
+    public void testListMisc() { interpTest("list-misc-test", "(23,0)"); }
+    public void testListIndex() { interpTest("list-index-test", "(17,0)"); }
+    public void testListFilter() {  interpTest("list-filter-test", "(2,0)");   }
+    public void testIntegers() { interpTest("integers-test", "(1,2)"); }
+    public void testIteration() { interpTest("iteration-test", "(1,2)"); }
+    public void testUnification() { interpTest("unification-test", "(2,0)"); }
+    public void testStrcmp() { interpTest("strcmp-test", "(16,0)"); }
+    public void testTermZip() { interpTest("term-zip-test", "(8,0)"); }
+    public void testEnvTraversal() { interpTest("env-traversal-test", "(2,0)"); }
+    public void testSets() { interpTest("sets-test", "(11,0)"); }
+    public void testTables() { interpTest("tables-test", "(50,0)"); }
+    public void testSubstitution() {  interpTest("substitution-test", "(1,0)");   }
 
-    public void testListBasic() { interpTest("list-basic-test" ,"[17,0]"); }
-    public void testStringMisc() { interpTest("string-misc-test", "[14,0]"); }
+    public void testListBasic() { interpTest("list-basic-test" ,"(17,0)"); }
+    public void testStringMisc() { interpTest("string-misc-test", "(14,0)"); }
 
-    public void testDynamicRulesLowlevel() { interpTest("dynamic-rules-lowlevel-test", "[46,0]"); }
-    public void testDynamicRulesHighlevel() { interpTest("dynamic-rules-highlevel-test", "[7,0]"); }
+    public void testDynamicRulesLowlevel() { interpTest("dynamic-rules-lowlevel-test", "(46,0)"); }
+    public void testDynamicRulesHighlevel() { interpTest("dynamic-rules-highlevel-test", "(7,0)"); }
 
 /*
  *  Relies on ANSI C and/or POSIX semantics
@@ -61,15 +61,15 @@ public class LibraryTest extends AbstractInterpreterTest {
 
 */
 
-    public void testFile() { interpTest("file-test", "[97,0]"); }
-    public void testString() { interpTest("string-test", "[55,0]"); }
-    public void testRename() { interpTest("rename-test", "[2,0]"); }
-    public void testApply() { interpTest("apply-test", "[4,0]"); }
-    public void testScopedFiniteMap() { interpTest("scoped-finite-map-test", "[5,0]"); }
-    public void testSimpleTraversal() { interpTest("simple-traversal-test", "[6,0]"); }
-    public void testCollect() { interpTest("collect-test", "[11,0]"); }
-    public void testParenthesize() { interpTest("parenthesize-test", "[4,0]"); }
-    public void testParseOptions() { interpTest("parse-options-test" ,"[2,0]"); }
+    public void testFile() { interpTest("file-test", "(97,0)"); }
+    public void testString() { interpTest("string-test", "(55,0)"); }
+    public void testRename() { interpTest("rename-test", "(2,0)"); }
+    public void testApply() { interpTest("apply-test", "(4,0)"); }
+    public void testScopedFiniteMap() { interpTest("scoped-finite-map-test", "(5,0)"); }
+    public void testSimpleTraversal() { interpTest("simple-traversal-test", "(6,0)"); }
+    public void testCollect() { interpTest("collect-test", "(11,0)"); }
+    public void testParenthesize() { interpTest("parenthesize-test", "(4,0)"); }
+    public void testParseOptions() { interpTest("parse-options-test" ,"(2,0)"); }
 
     public void interpTest(String test, String result) {
         // FIXME tuplify 'result'
