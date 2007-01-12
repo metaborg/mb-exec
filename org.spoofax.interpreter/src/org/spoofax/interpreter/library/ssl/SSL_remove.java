2990a11e65ed0cd1d187318dfed2d512cf48f6a2
Karl Trygve Kalleberg <karltk@gmail.com>
2007-01-12 23:07:53 +0000

    Added SSL_remove.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16416

---
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/library/ssl/SSL_remove.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 12. jan.. 2007
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package org.spoofax.interpreter.library.ssl;
+
+import java.io.File;
+import java.util.List;
+
+import org.spoofax.interpreter.IConstruct;
+import org.spoofax.interpreter.IContext;
+import org.spoofax.interpreter.InterpreterException;
+import org.spoofax.interpreter.Tools;
+import org.spoofax.interpreter.library.AbstractPrimitive;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+
+public class SSL_remove extends AbstractPrimitive {
+
+    SSL_remove() {
+        super("SSL_remove", 0, 1);
+    }
+    
+    @Override
+    public boolean call(IContext env, List<IConstruct> svars, IStrategoTerm[] tvars)
+            throws InterpreterException {
+        
+        if(!Tools.isTermString(tvars[0]))
+            return false;
+        
+        String fn = Tools.asJavaString(tvars[0]);
+        
+        return new File(fn).delete();
+    }
+
+}
