15ebe666f0bab4ae3537efe4d2a1aaeb4539e614
Karl Trygve Kalleberg <karltk@gmail.com>
2007-01-12 23:41:31 +0000

    Added support for loading multiple program files.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16422

--- org.spoofax.interpreter/src/org/spoofax/interpreter/Main.java
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/Main.java
@@ -3,37 +3,39 @@ package org.spoofax.interpreter;
 import java.io.IOException;
 
 import org.spoofax.DebugUtil;
-import org.spoofax.interpreter.terms.IStrategoTerm;
 
 public class Main {
 
     public static void main(String args[]) throws IOException {
 
         Interpreter itp = new Interpreter();
-        String file = "";
+        String[] files = null;
         boolean waitForProfiler = false;
 
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("--debug")) {
                 DebugUtil.setDebug(true);
             } else if (args[i].equals("-i")) {
-                file = args[i + 1];
+                files = args[i + 1].split(",");
             } else if (args[i].equals("--wait-for-profiler")) {
                 waitForProfiler = true;
             }
         }
 
         try {
-            itp.load(file);
+            for(String fn : files) {
+                System.out.println("Loading " + fn);
+                itp.load(fn);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterpreterException e) {
             e.printStackTrace();
         }
 
-        IStrategoTerm inp = itp.getFactory().makeList();
         try {
-            itp.setCurrent(inp);
+            itp.setCurrent(itp.getFactory().makeList());
+            
             boolean r = itp.invoke("main_0_0");
             if(r) {
                 System.out.println("" + itp.current());
