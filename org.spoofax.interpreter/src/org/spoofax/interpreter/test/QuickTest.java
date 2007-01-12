f0675d3f991beeac9700ceb14507d3b82316f66c
Karl Trygve Kalleberg <karltk@gmail.com>
2007-01-12 14:58:01 +0000

    Added test for quick&dirty testing.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16398

---
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/test/QuickTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created on 23.nov.2005
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package org.spoofax.interpreter.test;
+
+import java.io.IOException;
+
+import org.spoofax.interpreter.InterpreterException;
+
+public class QuickTest extends AbstractLanguageTest {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp("tests/data/");
+    }
+    
+    public void testQuick() throws IOException, InterpreterException {
+        interpTest("quick", factory.parseFromString("()"), factory.parseFromString("()"));
+    }
+}
