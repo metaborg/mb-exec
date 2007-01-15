b8c7a2c5f828b2832e3c674f0698aa3b6289d4a3
Valentin David <valentin.david@gmail.com>
2007-01-15 11:16:17 +0000

    * src/org/spoofax/interpreter/stratego/Some.java: Implement it. * tests/data/compiler/test1.str: Delete imports.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter/; revision=16441

--- org.spoofax.interpreter/src/org/spoofax/interpreter/stratego/Some.java
+++ org.spoofax.interpreter/src/org/spoofax/interpreter/stratego/Some.java
@@ -7,8 +7,15 @@
  */
 package org.spoofax.interpreter.stratego;
 
+import org.spoofax.DebugUtil;
 import org.spoofax.interpreter.IContext;
 import org.spoofax.interpreter.InterpreterException;
+import org.spoofax.interpreter.Tools;
+import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoConstructor;
+import org.spoofax.interpreter.terms.IStrategoList;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.IStrategoTuple;
 
 public class Some extends Strategy {
 
@@ -18,9 +25,84 @@ public class Some extends Strategy {
         this.body = body;
     }
 
-    public boolean eval(IContext e) throws InterpreterException {
-        throw new InterpreterException("Unimplemented");
+    public boolean eval(IContext env) throws InterpreterException {
+        if (DebugUtil.isDebugging()) {
+            debug("One.eval() - ", env.current());
+        }
+
+        IStrategoTerm t = env.current();
+
+        switch (t.getTermType()) {
+            case IStrategoTerm.INT:
+                return DebugUtil.traceReturn(true, env.current(), this);
+            case IStrategoTerm.REAL:
+                return DebugUtil.traceReturn(true, env.current(), this);
+            case IStrategoTerm.STRING:
+                return DebugUtil.traceReturn(true, env.current(), this);
+            case IStrategoTerm.APPL:
+                return DebugUtil.traceReturn(evalSome(env, (IStrategoAppl)t), env.current(), this);
+            case IStrategoTerm.LIST:
+                return DebugUtil.traceReturn(evalSome(env, (IStrategoList)t), env.current(), this);
+            case IStrategoTerm.TUPLE:
+                return DebugUtil.traceReturn(evalSome(env, (IStrategoTuple)t), env.current(), this);
+            default:
+                throw new InterpreterException("Unknown ATerm type " + t.getTermType());
+        }
     }
+
+	private boolean evalSome(IContext env, IStrategoList list) throws InterpreterException {
+        IStrategoTerm[] l = new IStrategoTerm[list.size()];
+        boolean success = false;
+
+        for(int i = 0, sz = list.getSubtermCount(); i < sz; i++) {
+        	l[i] = list.get(i);
+        	env.setCurrent(Tools.termAt(list, i));
+            if(body.eval(env)) {
+            	l[i] = env.current();
+            	success = true;
+            }
+        }
+
+    	IStrategoList t2 = env.getFactory().makeList(l);            
+        env.setCurrent(t2);
+        return success;
+	}
+
+	private boolean evalSome(IContext env, IStrategoTuple list) throws InterpreterException {
+        IStrategoTerm[] l = new IStrategoTerm[list.size()];
+        boolean success = false;
+
+        for(int i = 0, sz = list.getSubtermCount(); i < sz; i++) {
+        	l[i] = list.get(i);
+        	env.setCurrent(Tools.termAt(list, i));
+            if(body.eval(env)) {
+            	l[i] = env.current();
+            	success = true;
+            }
+        }
+
+    	IStrategoList t2 = env.getFactory().makeList(l);            
+        env.setCurrent(t2);
+        return success;
+	}
+
+	private boolean evalSome(IContext env, IStrategoAppl t) throws InterpreterException {
+		IStrategoConstructor ctor = t.getConstructor();
+		IStrategoTerm[] list = t.getArguments();
+        boolean success = false;
+
+        for(int i = 0, sz = list.length; i < sz; i++) {
+        	env.setCurrent(list[i]);
+            if(body.eval(env)) {
+            	list[i] = env.current();
+            	success = true;
+            }
+        }
+
+    	IStrategoAppl t2 = env.getFactory().makeAppl(ctor, list);            
+        env.setCurrent(t2);
+        return success;
+	}
 
     public void prettyPrint(StupidFormatter sf) {
         sf.append("Some(");
