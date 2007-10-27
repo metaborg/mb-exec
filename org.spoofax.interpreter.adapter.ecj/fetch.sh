770bf9fc03647bdfade7c237fb150728de090cc6
Karl Trygve Kalleberg <karltk@gmail.com>
2007-10-27 15:11:08 +0000

    Fetcher should now work with Eclipse >=3.2. Rewrote shell scripts to use fully versioned jars. Should probably generate jar list.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.adapter.ecj/; revision=17346

--- org.spoofax.interpreter.adapter.ecj/fetch.sh
+++ org.spoofax.interpreter.adapter.ecj/fetch.sh
@@ -2,16 +2,25 @@
 
 ECLIPSE_HOME=${ECLIPSE_HOME:-${HOME}/apps/eclipse-3.3}
 
-jars="org.eclipse.core.contenttype_3.2.0.v20060603.jar \
-	org.eclipse.equinox.preferences_3.2.1.R32x_v20060717.jar \
-	org.eclipse.core.jobs_3.2.0.v20060603.jar \
-	org.eclipse.equinox.registry_3.2.1.R32x_v20060814.jar \
-	org.eclipse.core.resources_3.2.2.R32x_v20061218.jar \
-	org.eclipse.jdt.core_3.2.3.v_686_R32x.jar \
-	org.eclipse.core.runtime_3.2.0.v20060603.jar \
-	org.eclipse.osgi_3.2.2.R32x_v20070118.jar \
-	org.eclipse.equinox.common_3.2.0.v20060603.jar"
+if [ ! -f ${ECLIPSE_HOME}/eclipse ] ; then
+	echo "The env var ECLIPSE_HOME must point to the directory of an Eclipse 3.3 installation " 2>&1
+	exit 1
+fi
+
+jars="org.eclipse.core.contenttype
+	org.eclipse.equinox.preferences
+	org.eclipse.core.jobs
+	org.eclipse.equinox.registry
+	org.eclipse.core.resources
+	org.eclipse.jdt.core
+	org.eclipse.core.runtime
+	org.eclipse.osgi
+	org.eclipse.equinox.common"
+
+function latest() {
+	ls ${ECLIPSE_HOME}/plugins/$1_*.jar | sort | tail -n 1
+}
 
 for x in ${jars} ; do 
-	cp ${ECLIPSE_HOME}/plugins/${x} lib/
+	cp $(latest $x) lib/
 done
