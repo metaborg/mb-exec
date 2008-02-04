5a3c40b66c5822216981c9a0efce70135177237b
Karl Trygve Kalleberg <karltk@gmail.com>
2008-02-04 23:17:01 +0000

    * Actually include the wrapper code as well?;)
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.adapter.ecj/; revision=17482

---
+++ org.spoofax.interpreter.adapter.ecj/src/org/spoofax/interpreter/adapter/ecj/WrappedITypeParameter.java
@@ -0,0 +1,43 @@
+/*
+ * Created on 9. okt.. 2006
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package org.spoofax.interpreter.adapter.ecj;
+
+import org.eclipse.jdt.core.ITypeParameter;
+import org.eclipse.jdt.core.JavaModelException;
+import org.spoofax.interpreter.terms.IStrategoConstructor;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+
+public class WrappedITypeParameter extends AbstractECJAppl {
+
+    private final ITypeParameter wrappee;
+    private final static IStrategoConstructor CTOR = new ASTCtor("TypeParameter", 1);
+    
+    WrappedITypeParameter(ITypeParameter wrappee) {
+        super(CTOR);
+        this.wrappee = wrappee;
+    }
+    
+    public ITypeParameter getWrappee() {
+        return wrappee;
+    }
+
+    @Override
+    public IStrategoTerm getSubterm(int index) {
+        switch(index) {
+        case 0:
+        	try {
+        	return ECJFactory.wrap(wrappee.getBounds());
+        	} catch(JavaModelException e) {
+        		e.printStackTrace();
+        		return None.INSTANCE;
+        	}
+        }
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+}
