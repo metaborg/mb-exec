9a5213a32aeee40fc28494cb9bea72e03714dfef
MaartjeDeJonge <maartjedejonge@gmail.com>
2009-06-29 12:01:20 +0000

    error recovery
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.adapter.ecj/; revision=19583

--- org.spoofax.interpreter.adapter.ecj/src/org/spoofax/ecjadapter/ParseJava.java
+++ org.spoofax.interpreter.adapter.ecj/src/org/spoofax/ecjadapter/ParseJava.java
@@ -9,9 +9,15 @@ package org.spoofax.ecjadapter;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -26,13 +32,19 @@ public class ParseJava {
     public static void main(String[] args) throws IOException, InterpreterException {
         
     	String inputFile = null;
-        
+    	String outputFile = null;
+    	boolean statmentsRecovery=false;
+    	
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("--debug")) {
                 DebugUtil.setDebug(true);
             } else if (args[i].equals("-i")) {
                 inputFile = args[i + 1];
-            } 
+            } else if (args[i].equals("-o")) {
+                outputFile = args[i + 1];
+            }else if (args[i].equals("-sr")) {
+                statmentsRecovery=true;
+            }
         }
 
         if(inputFile == null) {
@@ -40,7 +52,7 @@ public class ParseJava {
             System.exit(2);
         }
 
-        IStrategoTerm r = parseJava(inputFile);
+        IStrategoTerm r = parseJava(inputFile, statmentsRecovery);
         if(r == null) {
         	System.err.println("Failed to open and read file " + inputFile);
         	System.exit(3);
@@ -48,12 +60,20 @@ public class ParseJava {
 
         InlinePrinter ip = new InlinePrinter();
         r.prettyPrint(ip);
-        System.out.println(ip.getString());
+        String output= ip.getString();
+        System.out.println(output);
+        
+        if(outputFile!=null){
+        	FileOutputStream ous = new FileOutputStream(outputFile);
+        	ous.write(output.getBytes());
+        }
     }
  
-    private static IStrategoTerm parseJava(String fileName) throws FileNotFoundException, IOException {
+    private static IStrategoTerm parseJava(String fileName, boolean useStatementsRecovery) throws FileNotFoundException, IOException {
     
     	ASTParser parser = ASTParser.newParser(AST.JLS3);
+    	parser.setStatementsRecovery(useStatementsRecovery);   	
+    	
     	parser.setSource(getBytes(fileName));
     
     	ASTNode ast = parser.createAST(null);
