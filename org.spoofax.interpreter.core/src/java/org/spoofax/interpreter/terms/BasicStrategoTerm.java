677a8c355f1bff09d9317d718a2ef1bf3554835f
Lennart Kats <lclkats@gmail.com>
2009-09-02 14:26:09 +0000

    Fixed annotation pretty printing.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.core/; revision=19916

--- org.spoofax.interpreter.core/src/java/org/spoofax/interpreter/terms/BasicStrategoTerm.java
+++ org.spoofax.interpreter.core/src/java/org/spoofax/interpreter/terms/BasicStrategoTerm.java
@@ -44,9 +44,9 @@ public abstract class BasicStrategoTerm implements IStrategoTerm, Cloneable {
         
         sb.append('{');
         sb.append(annos.get(0));
-        for (int i = 1; i < annos.size(); i++) {
-            sb.append(",");
-            sb.append(annos.toString());
+        for (annos = annos.tail(); !annos.isEmpty(); annos = annos.tail()) {
+            sb.append(',');
+            sb.append(annos.head().toString());         
         }
         sb.append('}');
     }
@@ -56,10 +56,10 @@ public abstract class BasicStrategoTerm implements IStrategoTerm, Cloneable {
         if (annos.size() == 0) return;
         
         pp.print("{");
-        annos.get(0).prettyPrint(pp);
-        for (int i = 1; i < annos.size(); i++) {
-            pp.print(", ");
-            pp.print(annos.toString());
+        annos.head().prettyPrint(pp);
+        for (annos = annos.tail(); !annos.isEmpty(); annos = annos.tail()) {
+            pp.print(",");
+            annos.head().prettyPrint(pp);           
         }
         pp.print("}");
     }
