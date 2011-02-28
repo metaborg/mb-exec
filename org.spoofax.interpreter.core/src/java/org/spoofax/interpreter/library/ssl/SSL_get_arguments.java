b26b00c62cdeb9fe96e0c953c8159db4f67c8c42
Lennart Kats <lclkats@gmail.com>
2011-02-28 10:49:11 +0000

    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.core/; revision=22226

--- org.spoofax.interpreter.core/src/java/org/spoofax/interpreter/library/ssl/SSL_get_arguments.java
+++ org.spoofax.interpreter.core/src/java/org/spoofax/interpreter/library/ssl/SSL_get_arguments.java
@@ -12,6 +12,7 @@ import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoPlaceholder;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.IStrategoTuple;
 
@@ -40,6 +41,10 @@ public class SSL_get_arguments extends AbstractPrimitive {
         case IStrategoTerm.TUPLE:
             IStrategoTuple tuple = (IStrategoTuple) tvars[0];
             env.setCurrent(env.getFactory().makeList(tuple.getAllSubterms()));
+            return true;
+        case IStrategoTerm.PLACEHOLDER:
+            IStrategoPlaceholder placeholder = (IStrategoPlaceholder) tvars[0];
+            env.setCurrent(placeholder.getTemplate());
             return true;
         default:
             throw new IllegalStateException("SSL_get_arguments failed for " + tvars[0]);
