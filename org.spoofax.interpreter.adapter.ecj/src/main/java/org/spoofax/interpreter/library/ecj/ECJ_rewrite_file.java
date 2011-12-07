0805e7ffb16c8c70c7428a0c1d38797473bfb156
Karl Trygve Kalleberg <karltk@gmail.com>
2011-12-07 16:34:41 +0000

    We use different rewriting strategies for the imports and the body (the types) now, to improve layout preservation.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.adapter.ecj/; revision=23711

--- org.spoofax.interpreter.adapter.ecj/src/main/java/org/spoofax/interpreter/library/ecj/ECJ_rewrite_file.java
+++ org.spoofax.interpreter.adapter.ecj/src/main/java/org/spoofax/interpreter/library/ecj/ECJ_rewrite_file.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2007-2011, Karl Trygve Kalleberg <karltk near strategoxt dot org>
+ *
+ * Licensed under the GNU Lesser Public License, v2.1
+ */
 package org.spoofax.interpreter.library.ecj;
 
 import java.util.ArrayList;
@@ -9,28 +14,32 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.text.edits.TextEdit;
 import org.spoofax.interpreter.adapter.ecj.ECJFactory;
 import org.spoofax.interpreter.adapter.ecj.WrappedASTNode;
+import org.spoofax.interpreter.adapter.ecj.WrappedImportDeclaration;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.CallT;
 import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 public class ECJ_rewrite_file extends AbstractPrimitive {
 
 	public ECJ_rewrite_file() {
-		super("ECJ_rewrite_file", 1, 1);
+		super("ECJ_rewrite_file", 2, 1);
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars)
 	throws InterpreterException {
@@ -71,13 +80,39 @@ public class ECJ_rewrite_file extends AbstractPrimitive {
 					if(term instanceof WrappedASTNode)
 						newTds.add(((WrappedASTNode)term).getWrappee());
 					else 
-						ecj.log("Rewriting resulted in an invalid tree");
+						ecj.log("Rewriting types resulted in an invalid tree");
 				} else {
 					newTds.add(td);
 				}
 			}
 			root.types().clear();
 			root.types().addAll(newTds);
+			
+			List newImports = new ArrayList();
+			env.setCurrent(ECJFactory.wrap(root.imports()));
+			CallT s = (CallT)svars[1];
+			if(s.evaluate(env)) {
+				final IStrategoTerm term = env.current();
+				if(term instanceof IStrategoList) {
+					for(IStrategoTerm t : ((IStrategoList)term).getAllSubterms()) {
+						if(t instanceof WrappedImportDeclaration) {
+							ImportDeclaration id = ((WrappedImportDeclaration)t).getWrappee();
+							// FIXME should this be handled elsewhere?
+							if(id.getAST() != root.getAST()) {
+								id = (ImportDeclaration) ASTNode.copySubtree(root.getAST(), id);
+							}
+							newImports.add(id);
+						} else {
+							ecj.log("Rewriting import did not give an ImportDeclaration");
+						}
+					}
+				} else {
+					ecj.log("Rewriting imports did not give a list");
+				}
+			}
+			root.imports().clear();
+			root.imports().addAll(newImports);
+			
 			//System.out.println("after: " + root);
 			TextEdit te = root.rewrite(document, cu.getJavaProject().getOptions(true));
 			te.apply(document);
