500b8bf183e6784715ad6eb0d67c9fcae91c5c1a
Karl Trygve Kalleberg <karltk@gmail.com>
2011-12-07 16:27:33 +0000

    Added a little bit of robustness when loading source from classfiles or compilation units.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.adapter.ecj/; revision=23709

--- org.spoofax.interpreter.adapter.ecj/src/main/java/org/spoofax/interpreter/library/ecj/ECJ_unbacked_compilation_unit_for_type.java
+++ org.spoofax.interpreter.adapter.ecj/src/main/java/org/spoofax/interpreter/library/ecj/ECJ_unbacked_compilation_unit_for_type.java
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2005-2011, Karl Trygve Kalleberg <karltk near strategoxt dot org>
  * 
- * Licensed under the GNU Lesser Public License, v2.1
+ * Licensed under the GNU Lesser General Public License, v2.1
  */
 package org.spoofax.interpreter.library.ecj;
 
@@ -23,20 +23,23 @@ public class ECJ_unbacked_compilation_unit_for_type extends AbstractPrimitive {
     public ECJ_unbacked_compilation_unit_for_type() {
         super("ECJ_unbacked_compilation_unit_for_type", 0, 1);
     }
-    
+
     @Override
     public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars)
             throws InterpreterException {
 
     	if(!ECJTools.isIType(tvars[0]))
             return false;
-        
+
         try {
             final IType t = ECJTools.asIType(tvars[0]);
             final ECJLibrary ecj = (ECJLibrary) env.getOperatorRegistry(ECJLibrary.REGISTRY_NAME);
             final ASTParser parser = ecj.getParser();
+            final String src = t.getCompilationUnit() == null ? t.getClassFile().getSource() : t.getCompilationUnit().getSource();
             parser.setResolveBindings(true);
-			parser.setSource(t.getSource().toCharArray());
+			parser.setSource(src.toCharArray());
+			parser.setProject(ecj.getCurrentJavaProject());
+			parser.setUnitName(t.getElementName());
 	        CompilationUnit cu = (CompilationUnit)parser.createAST(null);
 	        env.setCurrent(ECJFactory.wrap(cu));
 	        return true;
