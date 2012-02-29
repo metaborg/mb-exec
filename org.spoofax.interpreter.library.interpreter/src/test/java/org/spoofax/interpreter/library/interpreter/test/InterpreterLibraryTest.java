17b8f350c953bb4b992d539495ac05ae5d1b5235
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-02-29 22:26:57 +0000

    test
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.interpreter/; revision=24443

--- org.spoofax.interpreter.library.interpreter/src/test/java/org/spoofax/interpreter/library/interpreter/test/InterpreterLibraryTest.java
+++ org.spoofax.interpreter.library.interpreter/src/test/java/org/spoofax/interpreter/library/interpreter/test/InterpreterLibraryTest.java
@@ -27,7 +27,7 @@ public class InterpreterLibraryTest extends AbstractInterpreterTest {
 	}
 
 	public void testInterpreter() throws IOException, InterpreterException {
-		interpTest("interpreter-tests", "(17,0)");
+		interpTest("interpreter-tests", "(19,0)");
 	}
 
 	public void interpTest(String test, String result) throws IOException, InterpreterException {
