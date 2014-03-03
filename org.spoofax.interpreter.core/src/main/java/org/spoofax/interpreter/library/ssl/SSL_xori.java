8bf7b12ba7ab5e354f12fe22e1e31bf5b5c00448
rmhartog <R.M.Hartog@student.tudelft.nl>
2014-03-03 11:48:36 +0100

    Added more bitwise operators to the SSL.

---
+++ org.spoofax.interpreter.core/src/main/java/org/spoofax/interpreter/library/ssl/SSL_xori.java
@@ -0,0 +1,37 @@
+/*
+ * Created on 08.aug.2005
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk near strategoxt.org>
+ * 
+ * Licensed under the GNU Lesser General Public License, v2.1
+ */
+package org.spoofax.interpreter.library.ssl;
+
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.core.Tools;
+import org.spoofax.interpreter.library.AbstractPrimitive;
+import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoInt;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+
+public class SSL_xori extends AbstractPrimitive {
+
+    protected SSL_xori() {
+        super("SSL_xori", 0, 2);
+    }
+    
+    @Override
+    public boolean call(IContext env, Strategy[] sargs, IStrategoTerm[] targs) throws InterpreterException {
+        
+        if(!Tools.isTermInt(targs[0]))
+            return false;
+        if(!Tools.isTermInt(targs[1]))
+            return false;
+
+        IStrategoInt a = (IStrategoInt) targs[0];
+        IStrategoInt b = (IStrategoInt) targs[1];
+        env.setCurrent(env.getFactory().makeInt(a.intValue() ^ b.intValue()));
+        return true;
+    }
+}
