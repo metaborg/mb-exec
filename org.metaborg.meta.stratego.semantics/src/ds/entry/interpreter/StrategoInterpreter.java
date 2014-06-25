86c14b2cc36698105fe45f633c076f55cc3c504d
Vlad Vergu <v.vergu@gmail.com>
2014-06-25 23:17:01 +0200

    Interpreter entry point with support for loading multiple ctrees.

--- org.metaborg.meta.stratego.semantics/src/ds/entry/interpreter/StrategoInterpreter.java
+++ org.metaborg.meta.stratego.semantics/src/ds/entry/interpreter/StrategoInterpreter.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.metaborg.meta.interpreter.framework.AValue;
+import org.metaborg.meta.interpreter.framework.INodeSource;
 import org.metaborg.meta.interpreter.framework.ImploderNodeSource;
 import org.metaborg.meta.interpreter.framework.NodeList;
 import org.metaborg.meta.interpreter.framework.NodeUtils;
@@ -31,31 +32,32 @@ public class StrategoInterpreter {
 			FileNotFoundException, IOException {
 		TermFactory tf = new TermFactory();
 
-		// 1. Read .ctree into ATerm
-		IStrategoTerm programATerm = new TermReader(tf)
-				.parseFromStream(new FileInputStream(args[0]));
-		System.out.println("PROGRAM " + programATerm);
+		// 0. determine the number of ctrees to load
+		int numCtrees = Integer.parseInt(args[0]);
+
+		// 1. load ctrees
+		PersistentMap<Object, Object> sdefs = new PersistentTreeMap<>();
+		for (int i = 1; i <= numCtrees; i++) {
+			String ctreeFile = args[i];
+			System.out.println("Reading ctree " + ctreeFile);
+			IStrategoTerm ctree = new TermReader(tf)
+					.parseFromStream(new FileInputStream(ctreeFile));
+			INodeSource ctreeSource = ctree
+					.getAttachment(ImploderAttachment.TYPE) != null ? new ImploderNodeSource(
+					ctree.getAttachment(ImploderAttachment.TYPE)) : null;
+			I_Module ctreeNode = new Generic_Module(ctreeSource, ctree);
+			System.out.println("Specializing ctree " + ctreeFile);
+			ctreeNode = (I_Module) NodeUtils.eagerReplacement(ctreeNode);
+			System.out.println("Loading ctree " + ctreeFile);
+			sdefs = new topdefs_1(ctreeSource, ctreeNode).exec_sdefs(sdefs).value;
+			System.out.println("Done processing " + ctreeFile);
+		}
+		System.out.println("Loaded " + sdefs.size() + " strategy definitions");
 
 		// 2. Read current term into ATerm
-		IStrategoTerm currentTerm = args.length > 1 ? new TermReader(tf)
-				.parseFromString(args[1]) : null;
+		IStrategoTerm currentTerm = args.length == numCtrees + 2 ? new TermReader(
+				tf).parseFromString(args[numCtrees + 1]) : null;
 		System.out.println("CURTERM " + currentTerm);
-
-		// 2. Construct the root node of the interpreter
-		I_Module programNode = new Generic_Module(
-				programATerm.getAttachment(ImploderAttachment.TYPE) != null ? new ImploderNodeSource(
-						programATerm.getAttachment(ImploderAttachment.TYPE))
-						: null, programATerm);
-		System.out.println("GEN PROGRAM NODE " + NodeUtils.toString(programNode));
-
-		// 2a. Eager specialize the program node
-		programNode = (I_Module) NodeUtils.eagerReplacement(programNode);
-		System.out.println("SPEC PROGRAM NODE " + NodeUtils.toString(programNode));
-		
-		// 3. Invoke the exec_defs method on the root node
-		com.github.krukow.clj_ds.PersistentMap<Object, Object> sdefs = new topdefs_1(
-				programNode.getSourceInfo(), programNode).exec_sdefs().value;
-		System.out.println("SDEFS " + sdefs);
 
 		// 4. Call the main_0_0 strategy
 		PersistentMap<Object, Object> env = new PersistentTreeMap<>();
