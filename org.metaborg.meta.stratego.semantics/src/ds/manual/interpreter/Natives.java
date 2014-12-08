1fe7c5c47b47b5237ddcda614ede1aca86206412
Vlad Vergu <v.vergu@gmail.com>
2014-12-08 15:59:45 +0200

    Ah. Actually push the strategy name on the stack tracer.

--- org.metaborg.meta.stratego.semantics/src/ds/manual/interpreter/Natives.java
+++ org.metaborg.meta.stratego.semantics/src/ds/manual/interpreter/Natives.java
@@ -13,68 +13,9 @@ import ds.generated.interpreter.I_STerm;
 
 public class Natives {
 
-	// public static boolean isATermInt_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoTerm.INT;
-	// }
-	//
-	// public static IStrategoInt asATermInt_1(IStrategoTerm t) {
-	// return (IStrategoInt) t;
-	// }
-	//
-	// public static boolean isATermString_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoTerm.STRING;
-	// }
-	//
-	// public static IStrategoString asATermString_1(IStrategoTerm t) {
-	// return (IStrategoString) t;
-	// }
-	//
-	// public static boolean isATermAppl_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoString.APPL;
-	// }
-	//
-	// public static IStrategoAppl asATermAppl_1(IStrategoTerm t) {
-	// return (IStrategoAppl) t;
-	// }
-	//
-	// public static boolean isATermList_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoTerm.LIST;
-	// }
-	//
-	// public static IStrategoList asATermList_1(IStrategoTerm t) {
-	// return (IStrategoList) t;
-	// }
-
 	public static int length_1(INodeList<I_STerm> ts) {
 		return ts.size();
 	}
-
-	// public static boolean isATermTuple_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoTerm.TUPLE;
-	// }
-	//
-	// public static IStrategoTuple asATermTuple_1(IStrategoTerm t) {
-	// return (IStrategoTuple) t;
-	// }
-
-	// public static AValue primCall_5(AutoInterpInteropContext context,
-	// String name, INodeList<I_Strategy> ass,
-	// INodeList<IStrategoTerm> ats_aterms, IStrategoTerm t) {
-	// AbstractPrimitive prim = context.lookupOperator(name);
-	// try {
-	// context.setCurrent(t);
-	// boolean result = prim.call(context, new Strategy[0],
-	// List2TARRAY_1(ats_aterms));
-	// if (result) {
-	// return new S_1(context.current());
-	// } else {
-	// return new F_0();
-	// }
-	// } catch (InterpreterException e) {
-	// throw new org.metaborg.meta.interpreter.framework.InterpreterException(
-	// "Primitive application failed", e);
-	// }
-	// }
 
 	public static IStrategoTerm importTerm_2(AutoInterpInteropContext context,
 			String p) {
@@ -88,16 +29,6 @@ public class Natives {
 		}
 	}
 
-	// @SuppressWarnings("unchecked")
-	// public static INodeList<IStrategoTerm> asNILofT_1(INodeList<?> l) {
-	// return (INodeList<IStrategoTerm>) l;
-	// }
-	//
-	// @SuppressWarnings("unchecked")
-	// public static INodeList<String> asNILofString_1(INodeList<?> l) {
-	// return (INodeList<String>) l;
-	// }
-
 	public static IStrategoTerm[] List2TARRAY_1(INodeList<IStrategoTerm> ts_) {
 		int size = ts_.size();
 		IStrategoTerm[] clone = new IStrategoTerm[size];
@@ -108,15 +39,6 @@ public class Natives {
 		}
 		return clone;
 	}
-
-	// public static PersistentMap<Object, Object> asD_1(
-	// PersistentMap<Object, Object> map) {
-	// return map;
-	// }
-	//
-	// public static boolean isNil_1(Object v) {
-	// return v == null;
-	// }
 
 	public static IStrategoList makeNil_1(ITermFactory tf) {
 		return tf.makeList();
@@ -131,7 +53,6 @@ public class Natives {
 	}
 
 	private static int counter = 0;
-
 
 	public static boolean isQuotedString_1(String s) {
 		return s.length() > 1 && s.charAt(0) == '"';
@@ -170,27 +91,14 @@ public class Natives {
 		return b1 || b2;
 	}
 
-	// @SuppressWarnings("unchecked")
-	// public static PersistentMap<Object, Object> asSHeap_1(Object map) {
-	// return (PersistentMap<Object, Object>) map;
-	// }
-
-	// public static boolean isATermReal_1(IStrategoTerm t) {
-	// return t.getTermType() == IStrategoTerm.REAL;
-	// }
-	//
-	// public static IStrategoReal asATermReal_1(IStrategoTerm t) {
-	// return (IStrategoReal) t;
-	// }
-
 	public static double parseReal_1(String s) {
 		return Double.parseDouble(s);
 	}
 
-	// @SuppressWarnings("unchecked")
-	// public static INodeList<I_Thunk> asListOfThunks_1(INodeList<?> l) {
-	// return (INodeList<I_Thunk>) l;
-	// }
+	public static boolean pushTraceElem_2(String s, StackTracer t) {
+		t.push(s);
+		return true;
+	}
 
 	public static boolean popOnFailure_1(StackTracer t) {
 		t.popOnFailure();
