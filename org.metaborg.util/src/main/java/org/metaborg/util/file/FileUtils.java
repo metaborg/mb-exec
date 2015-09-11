338b3b48c275d4deef0d9c2e793108302e731359
Gabriel Konat <gabrielkonat@gmail.com>
2015-09-11 11:04:18 -0700

    Add extra utilities

--- org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
@@ -1,11 +1,26 @@
 package org.metaborg.util.file;
 
 import java.io.File;
+import java.util.Collection;
 
 import org.apache.commons.vfs2.FileObject;
 
+import com.google.common.collect.Lists;
+
 public class FileUtils {
     public static File toFile(FileObject fileObject) {
-        return new File(fileObject.getName().getPath());
+        return new File(toPath(fileObject));
+    }
+    
+    public static Iterable<File> toFiles(Iterable<FileObject> fileObjects) {
+        final Collection<File> files = Lists.newLinkedList();
+        for(FileObject fileObject : fileObjects) {
+            files.add(toFile(fileObject));
+        }
+        return files;
+    }
+    
+    public static String toPath(FileObject fileObject) {
+        return fileObject.getName().getPath();
     }
 }
