e3fd9cf915b238cd42fe7770dc80eee335511f7d
Gabriel Konat <gabrielkonat@gmail.com>
2015-12-20 17:41:35 +0100

    Fix file utilities returning wrong Files on Windows. Added sneaky throws class for throwing exceptions without requiring checking.

--- org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
@@ -1,15 +1,25 @@
 package org.metaborg.util.file;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 
+import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileObject;
 
 import com.google.common.collect.Lists;
 
 public class FileUtils {
     public static File toFile(FileObject fileObject) {
-        return new File(toPath(fileObject));
+        try {
+            final FileName name = fileObject.getName();
+            final URI uri = new URI(name.getURI());
+            final File file = new File(uri);
+            return file;
+        } catch(URISyntaxException e) {
+            throw new RuntimeException("Could not convert FileObject to File", e);
+        }
     }
     
     public static Iterable<File> toFiles(Iterable<FileObject> fileObjects) {
@@ -20,7 +30,11 @@ public class FileUtils {
         return files;
     }
     
-    public static String toPath(FileObject fileObject) {
-        return fileObject.getName().getPath();
+    public static URI toURI(FileObject fileObject) {
+        try {
+            return new URI(fileObject.getName().getURI());
+        } catch(URISyntaxException e) {
+            throw new RuntimeException("Could not convert FileObject to URI", e);
+        }
     }
 }
