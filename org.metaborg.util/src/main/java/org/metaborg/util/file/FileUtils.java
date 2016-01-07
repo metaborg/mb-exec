f18558042ec8fd835ec4a14bc569284fe4186879
Gabriel Konat <gabrielkonat@gmail.com>
2016-01-07 18:29:40 +0100

    Add path sanitization utility.

--- org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/file/FileUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileObject;
@@ -11,6 +12,9 @@ import org.apache.commons.vfs2.FileObject;
 import com.google.common.collect.Lists;
 
 public class FileUtils {
+    private static Pattern sanitizePattern = Pattern.compile("[^a-zA-Z0-9.-]");
+
+
     public static File toFile(FileObject fileObject) {
         final URI uri = toURI(fileObject);
         final File file = new File(uri);
@@ -33,5 +37,9 @@ public class FileUtils {
         } catch(URISyntaxException e) {
             throw new RuntimeException("Could not convert FileObject to URI", e);
         }
+    }
+
+    public static String sanitize(String path) {
+        return sanitizePattern.matcher(path).replaceAll("_");
     }
 }
