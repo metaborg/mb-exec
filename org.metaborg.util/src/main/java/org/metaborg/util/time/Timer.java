0a753f592d9d9dada3fd57d1dee7232d9f54d09d
Gabriel Konat <gabrielkonat@gmail.com>
2016-03-10 17:01:15 +0100

    Add some documentation.

--- org.metaborg.util/src/main/java/org/metaborg/util/time/Timer.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/time/Timer.java
@@ -7,45 +7,56 @@ import java.lang.management.ThreadMXBean;
  * Simple time measurement functionality.
  */
 public class Timer {
-	/** ThreadMXBean for measuring CPU time. **/
-	private final ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+    /** ThreadMXBean for measuring CPU time. **/
+    private final ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
 
-	/** If precise CPU time measurements are available. **/
-	private final boolean canLogCPUTime;
+    /** If precise CPU time measurements are available. **/
+    private final boolean canLogCPUTime;
 
-	/** Last starting time since start was called. **/
-	private long startTime = 0;
-
-	
-	public Timer() {
-	    this(false);
-	}
-	
-	public Timer(boolean start) {
-		canLogCPUTime = mxBean.isThreadCpuTimeSupported();
-		if(canLogCPUTime)
-			mxBean.setThreadCpuTimeEnabled(true);
-		if(start)
-		    start();
-	}
+    /** Last starting time since start was called. **/
+    private long startTime = 0;
 
 
-	public void start() {
-		startTime = time();
-	}
+    public Timer() {
+        this(false);
+    }
 
-	public long stop() {
-		return time() - startTime;
-	}
+    public Timer(boolean start) {
+        canLogCPUTime = mxBean.isThreadCpuTimeSupported();
+        if(canLogCPUTime)
+            mxBean.setThreadCpuTimeEnabled(true);
+        if(start)
+            start();
+    }
 
-	public void reset() {
-		startTime = 0;
-	}
 
-	private long time() {
-		if(canLogCPUTime)
-			return mxBean.getCurrentThreadCpuTime();
-		else
-			return System.nanoTime();
-	}
+    /**
+     * Starts the timer, noting the current time.
+     */
+    public void start() {
+        startTime = time();
+    }
+
+    /**
+     * @return The duration, in nanoseconds, between the call to {@link #start()} and this invocation. This method can
+     *         be called multiple times after one {@link #start()} invocation.
+     */
+    public long stop() {
+        return time() - startTime;
+    }
+
+    /**
+     * Resets the timer, forgetting the time noted when {@link #start()} was called.
+     */
+    public void reset() {
+        startTime = 0;
+    }
+
+
+    private long time() {
+        if(canLogCPUTime)
+            return mxBean.getCurrentThreadCpuTime();
+        else
+            return System.nanoTime();
+    }
 }
