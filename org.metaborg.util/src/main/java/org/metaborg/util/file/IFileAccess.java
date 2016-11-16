10b91bbd272d5e4179ef14c4524a0ec1db8ae9e5
Gabriel Konat <gabrielkonat@gmail.com>
2016-11-16 17:01:24 +0100

    Document IFileAccess interface.

--- org.metaborg.util/src/main/java/org/metaborg/util/file/IFileAccess.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/file/IFileAccess.java
@@ -2,8 +2,24 @@ package org.metaborg.util.file;
 
 import org.apache.commons.vfs2.FileObject;
 
+/**
+ * Interface for registering file access.
+ */
 public interface IFileAccess {
-    void read(FileObject file);
+    /**
+     * Indicate that given file, or (non-recursive) contents of given directory, are going to be read. Call before
+     * reading.
+     * 
+     * @param resource
+     *            File or directory going to be read.
+     */
+    void read(FileObject resource);
 
+    /**
+     * Indicate that given file has been written to. Call after writing.
+     * 
+     * @param file
+     *            File that has been written to.
+     */
     void write(FileObject file);
 }
