94f093f1a7a724f209b5a0b2baf1d517c9bbbc52
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-02-09 18:34:11 +0100

    Add some handy defaults.

--- org.metaborg.util/src/main/java/org/metaborg/util/functions/Predicate1.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/functions/Predicate1.java
@@ -5,4 +5,12 @@ public interface Predicate1<T> {
 
     boolean test(T t);
 
+    public static <T> Predicate1<T> never() {
+        return t -> false;
+    }
+
+    public static <T> Predicate1<T> always() {
+        return t -> true;
+    }
+
 }
