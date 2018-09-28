6f5938d353c95f7ad2ccce11afcfec8794ca5487
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-09-28 15:04:07 +0200

    Add sequencing utility function.

--- org.metaborg.util/src/main/java/org/metaborg/util/unit/Unit.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/unit/Unit.java
@@ -2,6 +2,9 @@ package org.metaborg.util.unit;
 
 import java.io.Serializable;
 
+import org.metaborg.util.functions.CheckedFunction0;
+import org.metaborg.util.functions.Function0;
+
 public final class Unit implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -9,6 +12,14 @@ public final class Unit implements Serializable {
     public static final Unit unit = new Unit();
 
     private Unit() {
+    }
+
+    public <T> T andThen(Function0<T> next) {
+        return next.apply();
+    }
+
+    public <T, E extends Throwable> T andThenOrThrow(CheckedFunction0<T, E> next) throws E {
+        return next.apply();
     }
 
     @Override public String toString() {
