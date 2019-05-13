6e0058edbef123f1e126207dc9fc3a3e3eccfcab
Gabriel Konat <gabrielkonat@gmail.com>
2019-05-13 11:26:26 +0200

    Add Gradle build files for org.metaborg.util.

---
+++ org.metaborg.util/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+  id("org.metaborg.gradle.config.junit-testing")
+}
+
+dependencies {
+  api(platform("org.metaborg:parent:$version"))
+
+  api("org.apache.commons:commons-vfs2")
+  api("commons-io:commons-io")
+  api("com.netflix.rxjava:rxjava-core")
+  api("com.google.guava:guava")
+  api("com.google.inject:guice")
+  api("org.slf4j:slf4j-api")
+
+  compileOnly("com.google.code.findbugs:jsr305")
+
+  testCompileOnly("junit:junit")
+  testCompileOnly("com.google.code.findbugs:jsr305")
+  testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.1.0")
+}
+
+// Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
+val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
+  from("$projectDir/src/test/resources")
+  into("$buildDir/classes/java/test")
+}
+tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "mb.exec"
+
+pluginManagement {
+  repositories {
+    // Get plugins from artifacts.metaborg.org, first.
+    maven("https://artifacts.metaborg.org/content/repositories/releases/")
+    maven("https://artifacts.metaborg.org/content/repositories/snapshots/")
+    // Required by several Gradle plugins (Maven central).
+    maven("https://artifacts.metaborg.org/content/repositories/central/") // Maven central mirror.
+    mavenCentral() // Maven central as backup.
+    // Get plugins from Gradle plugin portal.
+    gradlePluginPortal()
+  }
+}
+
+include("org.metaborg.util")
