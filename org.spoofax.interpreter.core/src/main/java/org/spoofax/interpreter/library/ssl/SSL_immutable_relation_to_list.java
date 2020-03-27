d6e5ccce003d3eda61d503b0623a29df0c07616a
Jeff Smits <mail@jeffsmits.net>
2020-03-27 14:56:07 +0100

    Fix bug with toArray

--- org.spoofax.interpreter.core/src/main/java/org/spoofax/interpreter/library/ssl/SSL_immutable_relation_to_list.java
+++ org.spoofax.interpreter.core/src/main/java/org/spoofax/interpreter/library/ssl/SSL_immutable_relation_to_list.java
@@ -8,8 +8,6 @@ import org.spoofax.interpreter.terms.ITermFactory;
 
 import io.usethesource.capsule.BinaryRelation;
 
-import static org.spoofax.terms.AbstractTermFactory.EMPTY_TERM_ARRAY;
-
 public class SSL_immutable_relation_to_list extends AbstractPrimitive {
 
     protected SSL_immutable_relation_to_list() {
@@ -25,7 +23,8 @@ public class SSL_immutable_relation_to_list extends AbstractPrimitive {
 
         final BinaryRelation.Immutable<IStrategoTerm, IStrategoTerm> relation =
             ((StrategoImmutableRelation) env.current()).backingRelation;
-        final IStrategoTerm[] array = relation.entrySet().toArray(EMPTY_TERM_ARRAY);
+        final IStrategoTerm[] array =
+            relation.tupleStream((k, v) -> factory.makeTuple(k, v)).toArray(IStrategoTerm[]::new);
 
         env.setCurrent(factory.makeList(array));
         return true;
