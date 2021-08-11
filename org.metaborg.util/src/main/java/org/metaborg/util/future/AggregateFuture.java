e0c70c0629c7375017dda664caa082ae45a2a27b
Aron Zwaan <aronzwaan@gmail.com>
2021-08-11 13:54:23 +0200

    Revert "Add short-circuiting options to aggregate future"
    This reverts commit 11b2f6464747af496a6c139d19d1eeb5a7bd1cc3.

--- org.metaborg.util/src/main/java/org/metaborg/util/future/AggregateFuture.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/future/AggregateFuture.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.metaborg.util.functions.Action1;
 import org.metaborg.util.functions.CheckedAction1;
 import org.metaborg.util.functions.CheckedAction2;
 import org.metaborg.util.functions.CheckedFunction1;
@@ -18,48 +17,37 @@ import org.metaborg.util.tuple.Tuple4;
 
 import com.google.common.collect.Lists;
 
-public class AggregateFuture<T, R> implements IFuture<R> {
+public class AggregateFuture<T> implements IFuture<List<T>> {
 
     private static final ILogger logger = LoggerUtils.logger(AggregateFuture.class);
-
-    @SuppressWarnings("rawtypes") private static final Function1 ID_REDUCER = v -> v;
 
     private Object lock = new Object();
     private volatile int remaining;
     private final T[] results;
-    private final CompletableFuture<R> result;
-    private final Function1<List<T>, R> reducer;
+    private final CompletableFuture<List<T>> result;
 
-    @SafeVarargs private AggregateFuture(Function1<List<T>, R> reducer,
-            IFuture<SC<? extends T, ? extends R>>... futures) {
-        this(reducer, Arrays.asList(futures));
+    @SafeVarargs public AggregateFuture(IFuture<? extends T>... futures) {
+        this(Arrays.asList(futures));
     }
 
-    private AggregateFuture(Function1<List<T>, R> reducer,
-            Iterable<? extends IFuture<SC<? extends T, ? extends R>>> futures) {
-        this(reducer, Lists.newArrayList(futures));
-    }
-
-    @SuppressWarnings("unchecked") private AggregateFuture(Function1<List<T>, R> reducer,
-            List<? extends IFuture<SC<? extends T, ? extends R>>> futures) {
-        final int count = futures.size();
+    @SuppressWarnings("unchecked") public AggregateFuture(Iterable<? extends IFuture<? extends T>> futures) {
+        final List<? extends IFuture<? extends T>> futureList = Lists.newArrayList(futures);
+        final int count = futureList.size();
         this.results = (T[]) new Object[count];
         this.result = new CompletableFuture<>();
-        this.reducer = reducer;
-        logger.trace("{} initialized with {}: {}", this, count, futures);
+        logger.trace("{} initialized with {}: {}", this, count, futureList);
 
         this.remaining = count;
         for(int i = 0; i < count; i++) {
             final int j = i;
-            futures.get(i).whenComplete((r, ex) -> {
+            futureList.get(i).whenComplete((r, ex) -> {
                 whenComplete(j, r, ex);
             });
         }
         fireIfComplete();
     }
 
-
-    private void whenComplete(int i, SC<? extends T, ? extends R> v, Throwable ex) {
+    private void whenComplete(int i, T r, Throwable ex) {
         // INVARIANT count > 0
         synchronized(lock) {
             if(remaining > 0) {
@@ -67,15 +55,9 @@ public class AggregateFuture<T, R> implements IFuture<R> {
                     logger.trace("{} completed {}: exception", this, i);
                     remaining = -1; // count will never be 0 and trigger completion
                 } else if(remaining > 0) {
-                    v.visit(t -> {
-                        logger.trace("{} completed {}: value", this, i);
-                        remaining -= 1;
-                        results[i] = t;
-                    }, r -> {
-                        logger.trace("{} completed {}: short-circuit", this, i);
-                        remaining = -1;
-                        result.complete(r);
-                    });
+                    logger.trace("{} completed {}: value", this, i);
+                    remaining -= 1;
+                    results[i] = r;
                 }
             }
         }
@@ -98,34 +80,36 @@ public class AggregateFuture<T, R> implements IFuture<R> {
             }
         }
         if(results != null) {
-            result.complete(reducer.apply(results));
+            result.complete(results);
         }
     }
 
     @Override public <U> IFuture<U>
-            handle(CheckedFunction2<? super R, Throwable, ? extends U, ? extends Throwable> handler) {
+            handle(CheckedFunction2<? super List<T>, Throwable, ? extends U, ? extends Throwable> handler) {
         return result.handle(handler);
     }
 
-    @Override public IFuture<R> whenComplete(CheckedAction2<? super R, Throwable, ? extends Throwable> handler) {
+    @Override public IFuture<List<T>>
+            whenComplete(CheckedAction2<? super List<T>, Throwable, ? extends Throwable> handler) {
         return result.whenComplete(handler);
     }
 
-    @Override public <U> IFuture<U> thenApply(CheckedFunction1<? super R, ? extends U, ? extends Throwable> handler) {
+    @Override public <U> IFuture<U>
+            thenApply(CheckedFunction1<? super List<T>, ? extends U, ? extends Throwable> handler) {
         return result.thenApply(handler);
     }
 
-    @Override public IFuture<Void> thenAccept(CheckedAction1<? super R, ? extends Throwable> handler) {
+    @Override public IFuture<Void> thenAccept(CheckedAction1<? super List<T>, ? extends Throwable> handler) {
         return result.thenAccept(handler);
     }
 
-    @Override public <U> IFuture<U>
-            thenCompose(CheckedFunction1<? super R, ? extends IFuture<? extends U>, ? extends Throwable> handler) {
+    @Override public <U> IFuture<U> thenCompose(
+            CheckedFunction1<? super List<T>, ? extends IFuture<? extends U>, ? extends Throwable> handler) {
         return result.thenCompose(handler);
     }
 
     @Override public <U> IFuture<U> compose(
-            CheckedFunction2<? super R, Throwable, ? extends IFuture<? extends U>, ? extends Throwable> handler) {
+            CheckedFunction2<? super List<T>, Throwable, ? extends IFuture<? extends U>, ? extends Throwable> handler) {
         return result.compose(handler);
     }
 
@@ -133,92 +117,31 @@ public class AggregateFuture<T, R> implements IFuture<R> {
         return result.isDone();
     }
 
-    @Override public java.util.concurrent.CompletableFuture<R> asJavaCompletion() {
+    @Override public java.util.concurrent.CompletableFuture<List<T>> asJavaCompletion() {
         return result.asJavaCompletion();
-    }
-
-    @SafeVarargs public static <T> IFuture<List<T>> of(IFuture<? extends T>... futures) {
-        return of(Arrays.asList(futures));
-    }
-
-    @SuppressWarnings("unchecked") public static <T> IFuture<List<T>>
-            of(Iterable<? extends IFuture<? extends T>> futures) {
-        final ArrayList<IFuture<SC<? extends T, ? extends List<T>>>> mappedFutures = new ArrayList<>();
-        futures.forEach(future -> mappedFutures.add(future.thenApply(SC::of)));
-        return new AggregateFuture<>(ID_REDUCER, mappedFutures);
-    }
-
-    @SafeVarargs public static <T, R> IFuture<R> ofShortCircuitable(Function1<List<T>, R> reduce,
-            IFuture<SC<? extends T, ? extends R>>... futures) {
-        return ofShortCircuitable(reduce, Arrays.asList(futures));
-    }
-
-    public static <T, R> IFuture<R> ofShortCircuitable(Function1<List<T>, R> reduce,
-            Iterable<? extends IFuture<SC<? extends T, ? extends R>>> futures) {
-        return new AggregateFuture<T, R>(reduce, futures);
     }
 
     @SuppressWarnings("unchecked") public static <T1, T2> IFuture<Tuple2<T1, T2>> apply(IFuture<T1> f1,
             IFuture<T2> f2) {
-        return of(f1, f2).thenApply(rs -> Tuple2.of((T1) rs.get(0), (T2) rs.get(1)));
+        return new AggregateFuture<>(f1, f2).thenApply(rs -> Tuple2.of((T1) rs.get(0), (T2) rs.get(1)));
     }
 
     @SuppressWarnings("unchecked") public static <T1, T2, T3> IFuture<Tuple3<T1, T2, T3>> apply(IFuture<T1> f1,
             IFuture<T2> f2, IFuture<T3> f3) {
-        return of(f1, f2, f3).thenApply(rs -> Tuple3.of((T1) rs.get(0), (T2) rs.get(1), (T3) rs.get(2)));
+        return new AggregateFuture<>(f1, f2, f3)
+                .thenApply(rs -> Tuple3.of((T1) rs.get(0), (T2) rs.get(1), (T3) rs.get(2)));
     }
 
     @SuppressWarnings("unchecked") public static <T1, T2, T3, T4> IFuture<Tuple4<T1, T2, T3, T4>> apply(IFuture<T1> f1,
             IFuture<T2> f2, IFuture<T3> f3, IFuture<T4> f4) {
-        return of(f1, f2, f3, f4)
+        return new AggregateFuture<>(f1, f2, f3, f4)
                 .thenApply(rs -> Tuple4.of((T1) rs.get(0), (T2) rs.get(1), (T3) rs.get(2), (T4) rs.get(3)));
     }
 
     public static <T, U> IFuture<List<U>> forAll(Iterable<T> items, Function1<T, IFuture<U>> toFuture) {
         final ArrayList<IFuture<U>> futures = new ArrayList<>();
         items.forEach(item -> futures.add(toFuture.apply(item)));
-        return of(futures);
-    }
-
-
-    public interface SC<T, R> {
-
-        void visit(Action1<T> onNoSC, Action1<R> onSC);
-
-        public static <T, R> SC<T, R> shortCircuit(R result) {
-            return new DoSC<>(result);
-        }
-
-        public static <T, R> SC<T, R> of(T result) {
-            return new NoSC<>(result);
-        }
-
-    }
-
-    private static final class DoSC<T, R> implements SC<T, R> {
-
-        private final R result;
-
-        public DoSC(R result) {
-            this.result = result;
-        }
-
-        @Override public void visit(Action1<T> onNoSC, Action1<R> onSC) {
-            onSC.apply(result);
-        }
-    }
-
-    private static final class NoSC<T, R> implements SC<T, R> {
-
-        private final T result;
-
-        public NoSC(T result) {
-            this.result = result;
-        }
-
-        @Override public void visit(Action1<T> onNoSC, Action1<R> onSC) {
-            onNoSC.apply(result);
-        }
+        return new AggregateFuture<>(futures);
     }
 
 }
--- org.metaborg.util/src/test/java/org/metaborg/util/future/AggregateFutureTest.java
+++ org.metaborg.util/src/test/java/org/metaborg/util/future/AggregateFutureTest.java
@@ -5,18 +5,17 @@ import static org.junit.Assert.assertTrue;
 import static org.metaborg.util.future.CompletableFuture.completedExceptionally;
 import static org.metaborg.util.future.CompletableFuture.completedFuture;
 
-import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
-import org.metaborg.util.functions.Function1;
-import org.metaborg.util.future.AggregateFuture.SC;
+import org.metaborg.util.future.AggregateFuture;
+import org.metaborg.util.future.CompletableFuture;
 
 public class AggregateFutureTest {
 
     @Test public void testAggregateOfNone() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af = AggregateFuture.of();
+        final AggregateFuture<Integer> af = new AggregateFuture<>();
         af.whenComplete((r, ex) -> {
             marker.set(ex == null && r.isEmpty());
         });
@@ -25,7 +24,7 @@ public class AggregateFutureTest {
 
     @Test public void testAggregateOfOneCompleted() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af = AggregateFuture.of(completedFuture(1));
+        final AggregateFuture<Integer> af = new AggregateFuture<>(completedFuture(1));
         af.whenComplete((r, ex) -> {
             marker.set(ex == null && r.size() == 1);
         });
@@ -34,7 +33,7 @@ public class AggregateFutureTest {
 
     @Test public void testAggregateOfOneFailed() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af = AggregateFuture.of(completedExceptionally(new Exception()));
+        final AggregateFuture<Integer> af = new AggregateFuture<>(completedExceptionally(new Exception()));
         af.whenComplete((r, ex) -> {
             marker.set(ex != null);
         });
@@ -44,7 +43,7 @@ public class AggregateFutureTest {
     @Test public void testAggregateOfOneFailed_Delayed() {
         final AtomicBoolean marker = new AtomicBoolean(false);
         final CompletableFuture<Integer> f = new CompletableFuture<>();
-        final IFuture<List<Integer>> af = AggregateFuture.of(f);
+        final AggregateFuture<Integer> af = new AggregateFuture<>(f);
         af.whenComplete((r, ex) -> {
             marker.set(ex != null);
         });
@@ -54,7 +53,7 @@ public class AggregateFutureTest {
 
     @Test public void testAggregateOfMultipleCompleted() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af = AggregateFuture.of(completedFuture(1), completedFuture(2));
+        final AggregateFuture<Integer> af = new AggregateFuture<>(completedFuture(1), completedFuture(2));
         af.whenComplete((r, ex) -> {
             marker.set(ex == null && r.size() == 2);
         });
@@ -65,7 +64,7 @@ public class AggregateFutureTest {
         final AtomicBoolean marker = new AtomicBoolean(false);
         final CompletableFuture<Integer> f1 = new CompletableFuture<>();
         final CompletableFuture<Integer> f2 = new CompletableFuture<>();
-        final IFuture<List<Integer>> af = AggregateFuture.of(f1, f2);
+        final AggregateFuture<Integer> af = new AggregateFuture<>(f1, f2);
         af.whenComplete((r, ex) -> {
             marker.set(ex == null && r.size() == 2);
         });
@@ -77,8 +76,8 @@ public class AggregateFutureTest {
 
     @Test public void testAggregateOfMultipleFailed() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af =
-                AggregateFuture.of(completedExceptionally(new Exception()), completedExceptionally(new Exception()));
+        final AggregateFuture<Integer> af =
+                new AggregateFuture<>(completedExceptionally(new Exception()), completedExceptionally(new Exception()));
         af.whenComplete((r, ex) -> {
             marker.set(ex != null);
         });
@@ -89,7 +88,7 @@ public class AggregateFutureTest {
         final AtomicBoolean marker = new AtomicBoolean(false);
         final CompletableFuture<Integer> f1 = new CompletableFuture<>();
         final CompletableFuture<Integer> f2 = new CompletableFuture<>();
-        final IFuture<List<Integer>> af = AggregateFuture.of(f1, f2);
+        final AggregateFuture<Integer> af = new AggregateFuture<>(f1, f2);
         af.whenComplete((r, ex) -> {
             marker.set(ex != null);
         });
@@ -102,8 +101,8 @@ public class AggregateFutureTest {
 
     @Test public void testAggregateOfMultipleCompletedAndFailed() {
         final AtomicBoolean marker = new AtomicBoolean(false);
-        final IFuture<List<Integer>> af =
-                AggregateFuture.of(completedFuture(42), completedExceptionally(new Exception()));
+        final AggregateFuture<Integer> af =
+                new AggregateFuture<>(completedFuture(42), completedExceptionally(new Exception()));
         af.whenComplete((r, ex) -> {
             marker.set(ex != null);
         });
@@ -115,7 +114,7 @@ public class AggregateFutureTest {
         final AtomicBoolean marker = new AtomicBoolean(false);
         final CompletableFuture<Integer> f1 = new CompletableFuture<>();
         final CompletableFuture<Integer> f2 = new CompletableFuture<>();
-        final IFuture<List<Integer>> af = AggregateFuture.of(f1, f2);
+        final AggregateFuture<Integer> af = new AggregateFuture<>(f1, f2);
         af.whenComplete((r, ex) -> {
             valMarker.set(r != null);
             marker.set(ex != null);
@@ -126,91 +125,6 @@ public class AggregateFutureTest {
         f1.completeExceptionally(new Exception());
         assertFalse(valMarker.get());
         assertTrue(marker.get());
-    }
-
-    @Test public void testResultOnShortCircuit() {
-        final AtomicBoolean marker = new AtomicBoolean(false);
-        final AtomicBoolean redMarker = new AtomicBoolean(false);
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f1 = new CompletableFuture<>();
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f2 = new CompletableFuture<>();
-        final Function1<List<Integer>, Integer> sum = intList -> {
-            redMarker.set(true);
-            return intList.stream().mapToInt(Integer::intValue).sum();
-        };
-        final IFuture<Integer> af = AggregateFuture.<Integer, Integer>ofShortCircuitable(sum, f1, f2);
-        af.whenComplete((r, ex) -> {
-            marker.set(ex == null && r == 42);
-        });
-        f2.complete(SC.shortCircuit(42));
-        assertTrue(marker.get());
-        assertFalse(redMarker.get());
-    }
-
-    @Test public void testResultNoShortCircuit() {
-        final AtomicBoolean redMarker = new AtomicBoolean(false);
-        final AtomicBoolean marker = new AtomicBoolean(false);
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f1 = new CompletableFuture<>();
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f2 = new CompletableFuture<>();
-        final Function1<List<Integer>, Integer> sum = intList -> {
-            redMarker.set(true);
-            return intList.stream().mapToInt(Integer::intValue).sum();
-        };
-        final IFuture<Integer> af = AggregateFuture.<Integer, Integer>ofShortCircuitable(sum, f1, f2);
-        af.whenComplete((r, ex) -> {
-            marker.set(ex == null && r == 42);
-        });
-        f1.complete(SC.of(20));
-        assertFalse(marker.get());
-        assertFalse(redMarker.get());
-        f2.complete(SC.of(22));
-        assertTrue(marker.get());
-        assertTrue(redMarker.get());
-    }
-
-    @Test public void testNoReduceOnException_First() {
-        final AtomicBoolean redMarker = new AtomicBoolean(false);
-        final AtomicBoolean exMarker = new AtomicBoolean(false);
-        final AtomicBoolean marker = new AtomicBoolean(false);
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f1 = new CompletableFuture<>();
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f2 = new CompletableFuture<>();
-        final Function1<List<Integer>, Integer> sum = intList -> {
-            redMarker.set(true);
-            return intList.stream().mapToInt(Integer::intValue).sum();
-        };
-        final IFuture<Integer> af = AggregateFuture.<Integer, Integer>ofShortCircuitable(sum, f1, f2);
-        af.whenComplete((r, ex) -> {
-            marker.set(ex == null);
-            exMarker.set(ex != null);
-        });
-        f1.completeExceptionally(new Exception());
-        assertFalse(marker.get());
-        assertTrue(exMarker.get());
-        assertFalse(redMarker.get());
-        f1.complete(SC.shortCircuit(22));
-        assertFalse(marker.get());
-        assertTrue(exMarker.get());
-        assertFalse(redMarker.get());
-    }
-
-    @Test public void testNoReduceOnException_Last() {
-        final AtomicBoolean redMarker = new AtomicBoolean(false);
-        final AtomicBoolean marker = new AtomicBoolean(false);
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f1 = new CompletableFuture<>();
-        final CompletableFuture<SC<? extends Integer, ? extends Integer>> f2 = new CompletableFuture<>();
-        final Function1<List<Integer>, Integer> sum = intList -> {
-            redMarker.set(true);
-            return intList.stream().mapToInt(Integer::intValue).sum();
-        };
-        final IFuture<Integer> af = AggregateFuture.<Integer, Integer>ofShortCircuitable(sum, f1, f2);
-        af.whenComplete((r, ex) -> {
-            marker.set(ex != null);
-        });
-        f1.complete(SC.of(20));
-        assertFalse(marker.get());
-        assertFalse(redMarker.get());
-        f2.completeExceptionally(new Exception());
-        assertTrue(marker.get());
-        assertFalse(redMarker.get());
     }
 
 }
