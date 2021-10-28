11fe95371fe1a399e7a340f845abd1d8ab9721b9
Gabriel Konat <gabrielkonat@gmail.com>
2021-10-28 10:42:22 +0200

    Make ThreadCancel Serializable.

--- org.metaborg.util/src/main/java/org/metaborg/util/task/ThreadCancel.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/task/ThreadCancel.java
@@ -1,9 +1,11 @@
 package org.metaborg.util.task;
 
+import java.io.Serializable;
+
 /**
  * Cancellation token implementation that cancels on thread interrupts.
  */
-public class ThreadCancel implements ICancel {
+public class ThreadCancel implements ICancel, Serializable {
     @Override public boolean cancelled() {
         return Thread.currentThread().isInterrupted();
     }
