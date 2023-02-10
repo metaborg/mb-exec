e209a4a6dae479ea89c88d6d90bf1edf0d7399e4
Jeff Smits <mail@jeffsmits.net>
2023-02-10 16:38:54 +0100

    Used capsule-like api for bag/bagmap. Add ImmutableCollection interface

--- org.metaborg.util/src/main/java/org/metaborg/util/collection/Bag.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/collection/Bag.java
@@ -1,35 +1,29 @@
 package org.metaborg.util.collection;
 
+import java.lang.reflect.Array;
 import java.util.AbstractCollection;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
- * A collection like a set, but which allows multiples. So unordered, but with an efficient contains check.
- * Size and iterator methods are not the most efficient, because internally duplicates are just counted.
+ * A multiset collection. Like a set, but allowing duplicates. So unordered, but with an efficient contains check.
+ * Iterator method is not the most efficient, because internally duplicates are just counted.
  * @param <E>
  */
-public class Bag<E> extends AbstractCollection<E> {
-    private Map<E, Integer> backingMap;
-
-    public Bag() {
-        this.backingMap = new HashMap<>();
-    }
-    public Bag(int initialCapacity) {
-        this.backingMap = new HashMap<>(initialCapacity);
-    }
+public abstract class Bag<E> implements Collection<E> {
+    protected abstract Map<E, Integer> backingMap();
 
     @Override public boolean isEmpty() {
-        return backingMap.isEmpty();
+        return backingMap().isEmpty();
     }
 
     @Override public boolean contains(Object o) {
         //noinspection SuspiciousMethodCalls
-        return backingMap.containsKey(o);
+        return backingMap().containsKey(o);
     }
 
     @Override public Object[] toArray() {
@@ -37,79 +31,202 @@ public class Bag<E> extends AbstractCollection<E> {
     }
 
     @SuppressWarnings("unchecked") @Override public <T> T[] toArray(T[] a) {
-        if(a.length <= size()) {
-            E[] b = (E[]) a;
-            int i = 0;
-            for(E e : this) {
-                b[i] = e;
-            }
-            return (T[]) b;
+        int size = size();
+        final T[] result;
+        if(a.length < size) {
+            result = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+        } else {
+            result = a;
         }
-        return (T[]) StreamSupport.stream(spliterator(), false).toArray();
-    }
 
-    /**
-     * @param e element whose presence in this collection is to be ensured
-     * @return whether the element was added (so always true)
-     */
-    @Override public boolean add(E e) {
-        backingMap.compute(e, (k, v) -> (v == null) ? 1 : v + 1);
-        return true;
-    }
-
-    /**
-     * @param o element to be removed from this collection, if present
-     * @return whether (one of the duplicates of) the element was removed
-     */
-    @SuppressWarnings("unchecked") @Override public boolean remove(Object o) {
-        //noinspection SuspiciousMethodCalls
-        if(!backingMap.containsKey(o)) {
-            return false;
+        int i = 0;
+        for(E e : this) {
+            result[i] = (T) e;
+            i++;
         }
-        backingMap.computeIfPresent((E) o, (k, v) -> (v == 1) ? null : v - 1);
-        return true;
+        return result;
     }
 
     @Override public boolean containsAll(Collection<?> c) {
+        if(c instanceof Bag) {
+            final Bag<E> other = (Bag<E>) c;
+            containsAll(other.backingMap().keySet());
+        }
         return c.stream().allMatch(this::contains);
     }
 
-    @Override public boolean addAll(Collection<? extends E> c) {
-        return c.stream().map(this::add).reduce(false, (a, b) -> a || b);
-    }
-
-    @Override public boolean removeAll(Collection<?> c) {
-        return c.stream().map(this::remove).reduce(false, (a, b) -> a || b);
-    }
-
-    @SuppressWarnings("unchecked") @Override public boolean retainAll(Collection<?> c) {
-        final Map<E, Integer> newBackingMap = new HashMap<>(c.size());
-        for(Object o : c) {
-            if(contains(o)) {
-                E e = (E) o;
-                newBackingMap.put(e, backingMap.get(e));
-            }
-        }
-        if(newBackingMap.size() < backingMap.size()) {
-            backingMap = newBackingMap;
-            return true;
-        }
-        return false;
-    }
-
-    @Override public void clear() {
-        backingMap.clear();
-    }
-
-    @Override public int size() {
-        return backingMap.values().stream().mapToInt(i -> i).sum();
-    }
-
     @Override public Iterator<E> iterator() {
-        return backingMap.entrySet().stream().flatMap(entry -> {
+        return backingMap().entrySet().stream().flatMap(entry -> {
             final E e = entry.getKey();
             return Stream.generate(() -> e).limit(entry.getValue());
         }).iterator();
     }
 
+
+    public static final class Immutable<E> extends Bag<E> implements ImmutableCollection<E> {
+        private Map<E, Integer> backingMap;
+        private int size;
+
+        private Immutable(Map<E, Integer> backingMap, int size) {
+            this.backingMap = backingMap;
+            this.size = size;
+        }
+
+        public static <E> Immutable<E> of() {
+            return new Immutable<>(Collections.emptyMap(), 0);
+        }
+
+        @Override protected Map<E, Integer> backingMap() {
+            return backingMap;
+        }
+
+        // INVARIANT: is equal to backingMap.values().stream().mapToInt(i -> i).sum();
+        @Override public int size() {
+            return size;
+        }
+
+        public Transient<E> asTransient() {
+            return new Transient<>(new HashMap<>(backingMap), size);
+        }
+    }
+
+    public static final class Transient<E> extends Bag<E> {
+        private Map<E, Integer> backingMap;
+        private int size = 0;
+        private Immutable<E> frozen;
+
+        private Transient(Map<E, Integer> backingMap, int size) {
+            this.backingMap = backingMap;
+        }
+
+        public static <E> Transient<E> of() {
+            return new Transient<>(new HashMap<>(), 0);
+        }
+
+        public static <E> Transient<E> withSizeEstimate(int initialCapacity) {
+            return new Transient<>(new HashMap<>(Sets.hashCapacity(initialCapacity)), 0);
+        }
+
+        public Map<E, Integer> backingMap() {
+            return backingMap;
+        }
+
+        /**
+         * @param e element whose presence in this collection is to be ensured
+         * @return whether the element was added (so always true)
+         */
+        @Override public boolean add(E e) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            backingMap.compute(e, (k, v) -> (v == null) ? 1 : v + 1);
+            size += 1;
+            return true;
+        }
+
+        /**
+         * @param o element to be removed from this collection, if present
+         * @return whether (one of the duplicates of) the element was removed
+         */
+        @SuppressWarnings("unchecked") @Override public boolean remove(Object o) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            //noinspection SuspiciousMethodCalls
+            if(!backingMap.containsKey(o)) {
+                return false;
+            }
+            backingMap.computeIfPresent((E) o, (k, v) -> (v == 1) ? null : v - 1);
+            size -= 1;
+            return true;
+        }
+
+        @Override public boolean addAll(Collection<? extends E> c) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            if(c instanceof Bag) {
+                final Bag<E> other = (Bag<E>) c;
+                for(Map.Entry<E, Integer> e : other.backingMap().entrySet()) {
+                    final E key = e.getKey();
+                    backingMap.put(key, backingMap.getOrDefault(key, 0) + e.getValue());
+                }
+            }
+            return c.stream().map(this::add).reduce(false, (a, b) -> a || b);
+        }
+
+        @Override public boolean removeAll(Collection<?> c) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            if(c instanceof Bag) {
+                final Bag<E> other = (Bag<E>) c;
+                for(Map.Entry<E, Integer> e : other.backingMap().entrySet()) {
+                    final E key = e.getKey();
+                    final int newDuplicates = backingMap.getOrDefault(key, 0) - e.getValue();
+                    if(newDuplicates <= 0) {
+                        backingMap.remove(key);
+                    } else {
+                        backingMap.put(key, newDuplicates);
+                    }
+                }
+            }
+            return c.stream().map(this::remove).reduce(false, (a, b) -> a || b);
+        }
+
+        @SuppressWarnings("unchecked") @Override public boolean retainAll(Collection<?> c) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            if(c instanceof Bag) {
+                final Bag<E> other = (Bag<E>) c;
+                boolean changed = false;
+                for(Map.Entry<E, Integer> e : backingMap.entrySet()) {
+                    final E key = e.getKey();
+                    final int newDuplicates = other.backingMap().getOrDefault(key, 0);
+                    if(newDuplicates <= 0) {
+                        backingMap.remove(key);
+                        changed = true;
+                    } else {
+                        changed |= backingMap.put(key, newDuplicates) == newDuplicates;
+                    }
+                }
+            }
+            final Map<E, Integer> newBackingMap = new HashMap<>(Sets.hashCapacity(c.size()));
+            int newSize = 0;
+            for(Object o : c) {
+                if(contains(o)) {
+                    E e = (E) o;
+                    final Integer duplicates = backingMap.get(e);
+                    newBackingMap.put(e, duplicates);
+                    newSize += duplicates;
+                }
+            }
+            if(newSize < size()) {
+                backingMap = newBackingMap;
+                size = newSize;
+                return true;
+            }
+            return false;
+        }
+
+        @Override public void clear() {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            backingMap.clear();
+        }
+
+        // INVARIANT: is equal to backingMap.values().stream().mapToInt(i -> i).sum();
+        @Override public int size() {
+            return size;
+        }
+
+        public Immutable<E> freeze() {
+            if(frozen == null) {
+                frozen = new Immutable<>(Collections.unmodifiableMap(backingMap), size);
+            }
+            return frozen;
+        }
+    }
 }
--- org.metaborg.util/src/main/java/org/metaborg/util/collection/BagMap.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/collection/BagMap.java
@@ -2,82 +2,179 @@ package org.metaborg.util.collection;
 
 import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 /**
- * A collection like a map, but which allows multiple values. Unordered, but with an efficient lookup.
+ * A multimap collection. Like a map, but allowing duplicate pairs. Unordered, but with an efficient lookup.
  * Size, iterator, and containsValue methods are not the most efficient, because internally duplicates are just counted.
  * @param <K>
  * @param <V>
  */
-public class BagMap<K,V> {
-    private final Map<K, Bag<V>> backingMap;
+public abstract class BagMap<K, V> {
+    // could be a capsule Map.Immutable instead, if you want to bother with that...
+    protected abstract Map<K, ? extends Bag<V>> backingMap();
 
-    public BagMap() {
-        this.backingMap = new HashMap<>();
-    }
+    public abstract Bag<V> get(K key);
 
     public int size() {
-        return backingMap.values().stream().mapToInt(Bag::size).sum();
+        return backingMap().values().stream().mapToInt(Bag::size).sum();
     }
 
     public boolean isEmpty() {
-        return backingMap.isEmpty();
+        return backingMap().isEmpty();
     }
 
-    public boolean containsKey(Object key) {
-        return backingMap.containsKey(key);
+    public boolean containsKey(K key) {
+        return backingMap().containsKey(key);
     }
 
-    public boolean containsValue(Object value) {
-        return backingMap.containsValue(value);
+    public boolean containsValue(V value) {
+        return backingMap().values().stream().anyMatch(b -> b.contains(value));
     }
 
-    public Bag<V> get(Object key) {
-        return backingMap.get(key);
-    }
-
-    public void put(K key, V value) {
-        backingMap.computeIfAbsent(key, k -> new Bag<>()).add(value);
-    }
-
-    public boolean remove(K key, V value) {
-        final AtomicBoolean present = new AtomicBoolean(false);
-        backingMap.computeIfPresent(key, (k, v) -> {
-            present.set(v.remove(value));
-            return v.isEmpty() ? null : v;
-        });
-        return present.get();
-    }
-
-    public Bag<V> removeAll(Object key) {
-        return backingMap.remove(key);
-    }
-
-    public void putAll(Map<? extends K, ? extends V> m) {
-        for(Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
-            put(entry.getKey(), entry.getValue());
-        }
-    }
-
-    public void clear() {
-        backingMap.clear();
-    }
-
-    public Set<K> keySet() {
-        return backingMap.keySet();
+    public Collection<K> keys() {
+        return backingMap().entrySet().stream().flatMap(e -> {
+            K key = e.getKey();
+            return e.getValue().stream().map(v -> key);
+        }).collect(Collectors.toList());
     }
 
     public Collection<V> values() {
-        return backingMap.values().stream().flatMap(Bag::stream).collect(Collectors.toList());
+        return backingMap().values().stream().flatMap(Bag::stream).collect(Collectors.toList());
     }
 
-    public Set<Map.Entry<K, V>> entrySet() {
-        return backingMap.entrySet().stream().flatMap(e -> e.getValue().stream().map(v -> new AbstractMap.SimpleImmutableEntry<>(e.getKey(), v))).collect(
-            Collectors.toSet());
+    public Collection<Map.Entry<K, V>> entries() {
+        return backingMap().entrySet().stream().flatMap(e -> {
+            K key = e.getKey();
+            return e.getValue().stream().map(v -> new AbstractMap.SimpleImmutableEntry<>(key, v));
+        }).collect(Collectors.toList());
+    }
+
+    public void forEach(BiConsumer<? super K, ? super V> action) {
+        for (Map.Entry<K, V> entry : entries()) {
+            action.accept(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static class Immutable<K, V> extends BagMap<K, V> {
+        private final Map<K, Bag.Transient<V>> backingMap;
+
+        private Immutable(Map<K, Bag.Transient<V>> backingMap) {
+            this.backingMap = backingMap;
+        }
+
+        public static <K, V> Immutable<K, V> of() {
+            return new Immutable(Collections.emptyMap());
+        }
+
+        @Override protected Map<K, ? extends Bag<V>> backingMap() {
+            return backingMap;
+        }
+
+        @Override public Bag.Immutable<V> get(K key) {
+            return backingMap.get(key).freeze();
+        }
+
+        public Transient<K, V> asTransient() {
+            Map<K, Bag.Transient<V>> transientBackingMap = new HashMap<>(Sets.hashCapacity(this.backingMap.size()));
+            for(Map.Entry<K, Bag.Transient<V>> e : this.backingMap.entrySet()) {
+                transientBackingMap.put(e.getKey(), e.getValue().freeze().asTransient());
+            }
+            return new Transient<>(transientBackingMap);
+        }
+    }
+
+    public static class Transient<K, V> extends BagMap<K, V> {
+        private final Map<K, Bag.Transient<V>> backingMap;
+        private BagMap.Immutable<K, V> frozen = null;
+
+        private Transient(Map<K, Bag.Transient<V>> backingMap) {
+            this.backingMap = backingMap;
+        }
+
+        public static <K, V> Transient<K, V> of() {
+            return new Transient(new HashMap());
+        }
+
+        public static <K, V> Transient<K, V> withSizeEstimate(int initialCapacity) {
+            return new Transient(new HashMap(Sets.hashCapacity(initialCapacity)));
+        }
+
+        @Override protected Map<K, ? extends Bag<V>> backingMap() {
+            return backingMap;
+        }
+
+        @Override public Bag.Transient<V> get(K key) {
+            return backingMap.get(key);
+        }
+
+        public void put(K key, V value) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            backingMap.computeIfAbsent(key, k -> Bag.Transient.of()).add(value);
+        }
+
+        public boolean remove(K key, V value) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            final AtomicBoolean present = new AtomicBoolean(false);
+            backingMap.computeIfPresent(key, (k, v) -> {
+                present.set(v.remove(value));
+                return v.isEmpty() ? null : v;
+            });
+            return present.get();
+        }
+
+        public Bag<V> removeAll(K key) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            final Bag<V> removed = backingMap.remove(key);
+            return removed != null ? removed : Bag.Transient.of();
+        }
+
+        public boolean removeAll(Collection<K> keys, V value) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            boolean changed = false;
+            for(K key : keys) {
+                changed |= remove(key, value);
+            }
+            return changed;
+        }
+
+        public void putAll(Map<? extends K, ? extends V> m) {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            for(Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
+                put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        public void clear() {
+            if(frozen != null) {
+                throw new IllegalStateException();
+            }
+            backingMap.clear();
+        }
+
+        public BagMap.Immutable<K, V> freeze() {
+            if(frozen == null) {
+                frozen = new BagMap.Immutable<>(Collections.unmodifiableMap(backingMap));
+            }
+            return frozen;
+        }
     }
 }
