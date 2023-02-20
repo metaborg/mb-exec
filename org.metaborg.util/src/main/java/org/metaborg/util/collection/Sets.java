6f4623a26bc4590f3c510a500fa64cf65f57c364
Jeff Smits <mail@jeffsmits.net>
2023-02-20 17:41:09 +0100

    Make Sets#hashCapacity more widely available

--- org.metaborg.util/src/main/java/org/metaborg/util/collection/Sets.java
+++ org.metaborg.util/src/main/java/org/metaborg/util/collection/Sets.java
@@ -320,7 +320,7 @@ public class Sets {
      * Returns a capacity that is sufficient to keep the map from being resized as long as it grows no
      * larger than expectedSize and the load factor is ≥ its default (0.75).
      */
-    static int hashCapacity(int expectedSize) {
+    public static int hashCapacity(int expectedSize) {
         if (expectedSize < 3) {
             return Integer.max(expectedSize, 2) + 1;
         }
