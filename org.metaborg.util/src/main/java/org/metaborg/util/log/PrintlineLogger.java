01f4d53e821d0daea4cf398894e8731256bc39be
Aron Zwaan <aronzwaan@gmail.com>
2023-08-31 11:42:06 +0200

    Add printline logger

---
+++ org.metaborg.util/src/main/java/org/metaborg/util/log/PrintlineLogger.java
@@ -0,0 +1,79 @@
+package org.metaborg.util.log;
+
+/**
+ * @deprecated Temporary device for debugging on buildfarm.
+ */
+@Deprecated
+public class PrintlineLogger {
+
+    private static final ILogger log = LoggerUtils.logger(PrintlineLogger.class);
+
+    private final String name;
+    private final boolean enabled;
+
+    public PrintlineLogger(String name) {
+        this.name = truncate(name);
+        enabled = !name.startsWith("mb.nabl2.solver.components");
+    }
+
+    public void debug(String message, Object... args) {
+        log(format(message, args), "[DEBUG] -");
+    }
+
+    public void debug(String message, Throwable ex, Object... args) {
+        log(format(message, args), "[DEBUG] -");
+        ex.printStackTrace(System.out);
+    }
+
+    public void info(String message, Object... args) {
+        log(format(message, args), "[INFO]  -");
+    }
+
+    public void info(String message, Throwable ex, Object... args) {
+        log(format(message, args), "[INFO]  -");
+        ex.printStackTrace(System.out);
+    }
+
+    public void warn(String message, Object... args) {
+        log(format(message, args), "[WARN]  -");
+    }
+
+    public void warn(String message, Throwable ex, Object... args) {
+        log(format(message, args), "[WARN]  -");
+        ex.printStackTrace(System.out);
+    }
+
+    public void error(String message, Object... args) {
+        log(format(message, args), "[ERROR] -");
+    }
+
+    public void error(String message, Throwable ex, Object... args) {
+        log(format(message, args), "[ERROR] -");
+        ex.printStackTrace(System.out);
+    }
+
+    private String format(String format, Object... args) {
+        return log.format(format, args);
+    }
+
+    private void log(String message, String level) {
+        if(!enabled) {
+            return;
+        }
+        System.out.printf("%s %-32s | %s%n", level, name, message);
+    }
+
+    private String truncate(String name) {
+        if(name.length() > 32) {
+            return name.substring(name.length() - 32);
+        } else {
+            return name;
+        }
+    }
+
+    public static PrintlineLogger logger(Class<?> clz) {
+        return new PrintlineLogger(clz.getCanonicalName());
+    }
+
+}
+
