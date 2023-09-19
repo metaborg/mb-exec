7a72882f636478009522f39cc7675e255f1ccbee
Aron Zwaan <aronzwaan@gmail.com>
2023-09-19 14:17:35 +0200

    Revert "Move TestableObserver to the one project where it's used: org.metaborg.core.test"
    This reverts commit e438f92495499c2eca77735a056298da804c8c3f.

---
+++ org.metaborg.util/src/main/java/org/metaborg/util/observable/TimestampedNotification.java
@@ -0,0 +1,13 @@
+package org.metaborg.util.observable;
+
+import io.reactivex.rxjava3.core.Notification;
+
+public class TimestampedNotification<T> {
+    public final Notification<T> notification;
+    public final long timestamp;
+
+    public TimestampedNotification(Notification<T> notification, long timestamp) {
+        this.notification = notification;
+        this.timestamp = timestamp;
+    }
+}
