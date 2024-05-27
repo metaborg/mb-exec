fe8096cbbbc0ed4df514bb069c996dcfbf001aae
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:37:27 +0200

    Reformat code to match Kotlin official style

--- org.spoofax.interpreter.core/build.gradle.kts
+++ org.spoofax.interpreter.core/build.gradle.kts
@@ -1,29 +1,29 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
+    id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.junit-testing")
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(compositeBuild("org.spoofax.terms"))
-  api(compositeBuild("org.spoofax.jsglr"))
-  implementation(compositeBuild("org.metaborg.util"))
-  implementation("commons-io:commons-io")
-  api("io.usethesource:capsule")
-  implementation(compositeBuild("org.metaborg.util"))
+    api(compositeBuild("org.spoofax.terms"))
+    api(compositeBuild("org.spoofax.jsglr"))
+    implementation(compositeBuild("org.metaborg.util"))
+    implementation("commons-io:commons-io")
+    api("io.usethesource:capsule")
+    implementation(compositeBuild("org.metaborg.util"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 
-  testCompileOnly("junit:junit")
-  testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testCompileOnly("junit:junit")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
 // Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
 val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
-  from("$projectDir/src/test/resources")
-  into("$buildDir/classes/java/test")
+    from("$projectDir/src/test/resources")
+    into("$buildDir/classes/java/test")
 }
 tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
--- org.spoofax.interpreter.library.xml/build.gradle.kts
+++ org.spoofax.interpreter.library.xml/build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.java-library")
 }
 
 sourceSets {
-  test {
-    java {
-      setSrcDirs(listOf<Any>()) // Disable tests, they cannot be compiled.
+    test {
+        java {
+            setSrcDirs(listOf<Any>()) // Disable tests, they cannot be compiled.
+        }
     }
-  }
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(compositeBuild("org.spoofax.terms"))
-  api(project(":org.spoofax.interpreter.core"))
+    api(compositeBuild("org.spoofax.terms"))
+    api(project(":org.spoofax.interpreter.core"))
 }
