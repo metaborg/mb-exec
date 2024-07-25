bb4cfb63b60d57f027fa2e10cbe076dcc5c87d0c
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-25 02:14:30 +0200

    Make repo build standalone

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+rootProject.name = "esv-project"
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.metaborg.convention.settings") version "latest.integration"
+}
+
+include("org.metaborg.util")
+include("org.metaborg.interpreter.core")
+include("org.metaborg.interpreter.library.java")
+include("org.metaborg.interpreter.library.xml")
+include("util-vfs2")
+
